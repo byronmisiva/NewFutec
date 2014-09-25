aab0c573a40b919f94fb9b2337d7bdd40616dc5e
byronherrera <herrera.byron@gmail.com>
2014-09-25 18:46:51 -0500

    mesagge

--- application/modules/contenido/views/header2.php
+++ application/modules/contenido/views/header2.php
@@ -2,7 +2,7 @@
     <?php echo $rotativas; ?>
 </div>
 <div class="col-md-4">
-    <div class="col-md-12">
+    <div class="col-md-12 margen0r">
         <?php echo $marcadorvivo; ?>
     </div>
     <div class="col-md-12 separador10">
--- application/modules/contenido/views/marcadorvivo.php
+++ application/modules/contenido/views/marcadorvivo.php
@@ -1,34 +1,61 @@
-<img src="assets/dummys/marcadorvivo.jpg">
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+<div id="carousel-marcadorenvivo" class="carousel slide marcadorenvivofondo" data-ride="carousel">
     <div class="carousel-inner">
         <div class="item active">
-            <div class="row ">
-                x
-                <div class="col-md-12 text-center">
+            <div class="row text-white">
+                <div class="col-md-12 text-center h5">
                     Botafogo 0 - 2 Santa Fe
                 </div>
-                <div class="col-md-6 text-center">
-                    Copa Sudamericana
-                    Canal 1
+                <div class="col-md-5 col-md-offset-1 h6 text-blue1">
+                    <p>Copa Sudamericana</p>
+
+                    <p>Canal 1</p>
                 </div>
-                <div class="col-md-6 text-center">
-                    Septiembre 19 20h30
-                    1er Tiempo
+                <div class="col-md-5 h6  text-blue1">
+                    <p>Septiembre 19 20h30</p>
+
+                    <p>1er Tiempo</p>
                 </div>
             </div>
         </div>
+        <div class="item ">
+            <div class="row text-white">
+                <div class="col-md-12 text-center h5">
+                    Botafogo 0 - s Santa Fe
+                </div>
+                <div class="col-md-5 col-md-offset-1 h6 text-blue1">
+                    <p>Copa Sudamericana</p>
 
-        <div class="item">
-            Texto 2
+                    <p>Canal 1</p>
+                </div>
+                <div class="col-md-5 h6  text-blue1">
+                    <p>Septiembre 19 20h30</p>
+
+                    <p>1er Tiempo</p>
+                </div>
+            </div>
         </div>
-        <div class="item">
-            Texto 3
+        <div class="item ">
+            <div class="row text-white">
+                <div class="col-md-12 text-center h5">
+                    Botafogo 0 - 23 Santa Fe
+                </div>
+                <div class="col-md-5 col-md-offset-1 h6 text-blue1">
+                    <p>Copa Sudamericana</p>
+
+                    <p>Canal 1</p>
+                </div>
+                <div class="col-md-5 h6  text-blue1">
+                    <p>Septiembre 19 20h30</p>
+
+                    <p>1er Tiempo</p>
+                </div>
+            </div>
         </div>
     </div>
-    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-        <
+    <a class="left carousel-control" href="#carousel-marcadorenvivo" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
     </a>
-    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-        >
+    <a class="right carousel-control" href="#carousel-marcadorenvivo" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right mev"></span>
     </a>
 </div>
