85d3972b4cdc6fd158e56003503e9ee4c45593d1
byronherrera <herrera.byron@gmail.com>
2014-09-26 12:18:21 -0500

    mesagge

--- application/modules/contenido/views/sidebar.php
+++ application/modules/contenido/views/sidebar.php
@@ -1,8 +1,57 @@
 <div class="col-md-12 separador10">
     <img src="assets/dummys/redes-buscar.jpg">
 </div>
-<div class="col-md-12 separador10">
+<div class="col-md-12 separador10  margen0r">
     <img src="assets/dummys/proxima-fecha.png">
+
+    <div class="panel-group" id="accordion">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+                        Próxima fecha
+                    </a>
+                </h4>
+            </div>
+            <div id="collapseOne" class="panel-collapse collapse in">
+                <div class="panel-body">
+
+                    <!-- Nav tabs -->
+                    <ul class="nav nav-tabs" role="tablist">
+                        <li class="active"><a href="#home" role="tab" data-toggle="tab">Serie A</a></li>
+                        <li><a href="#profile" role="tab" data-toggle="tab">Serie B</a></li>
+                        <li><a href="#messages" role="tab" data-toggle="tab">Copa Libertadores</a></li>
+
+                    </ul>
+
+                    <!-- Tab panes -->
+                    <div class="tab-content">
+                        <div class="tab-pane active" id="home">Contenido Serie A</div>
+                        <div class="tab-pane" id="profile">Contenido Serie B</div>
+                        <div class="tab-pane" id="messages">Contenido Copa Libertadores</div>
+
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+                        Resultados
+                    </a>
+                </h4>
+            </div>
+            <div id="collapseTwo" class="panel-collapse collapse">
+                <div class="panel-body">
+                    Contenidos de  resultados
+                </div>
+            </div>
+        </div>
+
+    </div>
 </div>
 <div class="col-md-12 separador10">
     <img src="assets/dummys/publi_300-250.jpg">
