a8bcfc224a84bd71b291726b9a0e121cc8675972
byronherrera <herrera.byron@gmail.com>
2014-10-17 13:22:56 -0500

    mesagge

--- application/modules/noticias/views/mininewssidebar.php
+++ application/modules/noticias/views/mininewssidebar.php
@@ -3,12 +3,24 @@
         <? echo $namesection; ?>
     </h4>
 </div>
-<?php foreach ($noticias as $noticia) {
-?>
-    <img src="http://www.futbolecuador.com/<?php echo $noticia->thumb3; ?>" class="img-responsive">
-    <?php echo $noticia->title; ?><br/>
-    <?php echo $noticia->lead; ?><br/>
-<?php } ?>
+
+<div class="row">
+    <?php foreach ($noticias as $noticia) {
+        ?>
+        <div class="col-md-12 lineseparador separador10">
+            <div class="row">
+                <div class="col-md-2 ">
+                    <img src="http://www.futbolecuador.com/<?php echo $noticia->thumb3; ?>" >
+                </div>
+                <div class="col-md-10  ">
+                    <h2><?php echo $noticia->title; ?></h2>
+                    <?php echo strip_tags($noticia->lead); ?><br/>
+                </div>
+            </div>
+        </div>
+
+    <?php } ?>
+</div>
 <div class="col-md-12 text-right fondoazul separador10">
     Más noticias
 </div>
