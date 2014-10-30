4ff1582c96ea35def7e4a5846322299520d7b4ba
byronherrera <herrera.byron@gmail.com>
2014-10-30 14:30:21 -0500

    message

--- application/modules/contenido/views/rotativas.php
+++ application/modules/contenido/views/rotativas.php
@@ -2,16 +2,24 @@
     <ul class="slides ">
         <?php
         foreach ($rotativasData as &$rotativa) {
+            $link = base_url() . 'site/noticia/' . $this->contenido->_urlFriendly($rotativa->title) . '/' . $rotativa->id;
             ?>
             <li data-thumb="http://www.futbolecuador.com/<?php echo $rotativa->thumbh80; ?>">
-                <img src="http://www.futbolecuador.com/<?php echo $rotativa->thumb500; ?>" alt="<?php echo str_replace('"', '', "$rotativa->title"); ?>"/>
+                <a href="<?php echo $link ?>">
+                    <img src="http://www.futbolecuador.com/<?php echo $rotativa->thumb500; ?>"
+                         alt="<?php echo str_replace('"', '', "$rotativa->title"); ?>"/>
+                </a>
 
                 <div class="content-text-rotativas">
+                    <a href="<?php echo $link ?>">
                     <div class="text-rotativas">
-                        <h2><?php echo $rotativa->title; ?></h2>
+                        <h2><?php echo $rotativa->title; ?> </h2>
 
-                        <h3><?php echo $rotativa->subtitle; ?> <?php //todo link a noticia ?></h3>
+                        <h3>
+                             <?php echo $rotativa->subtitle; ?>
+                        </h3>
                     </div>
+                    </a>
                 </div>
 
             </li>
--- application/modules/story/views/noticiaabierta.php
+++ application/modules/story/views/noticiaabierta.php
@@ -1,26 +1,33 @@
-<?php echo $noticia->origen; ?>
-<?php echo $noticia->created; ?>
-<?php echo $noticia->title; ?>
-<?php echo $noticia->subtitle; ?>
-<?php echo $noticia->lead; ?>
-<?php echo $noticia->body; ?>
-<?php echo $noticia->category_id    ; ?>
-
-<?php echo $noticia->thumb400; ?>
-<?php echo $noticia->image_name; ?>
-
-<?php echo $noticia->tags[0]->name; ?>
-
-
-<div class="panel-heading backcuadros">
-    <h4 class="panel-title">
-    </h4>
+<img src="http://www.futbolecuador.com/<?php echo $noticia->thumb400; ?>" alt="<?php echo $noticia->image_name; ?>">
+<div class="row clearfix news-open">
+    <div class="col-md-12 column text-news-date">
+            <?php setlocale(LC_ALL, "es_ES");
+            echo $noticia->origen . ", ".  date("l d F Y", strtotime($noticia->created)); ?>
+    </div>
+    <div class="col-md-12 column ">
+       <h1><?php echo $noticia->title; ?></h1>
+    </div>
+    <div class="col-md-12 column ">
+       <h2><?php echo $noticia->subtitle; ?></h2>
+    </div>
+    <div class="col-md-12 column ">
+        Redes
+    </div>
+    <div class="col-md-12 column ">
+        <?php echo html_entity_decode($noticia->lead); ?>
+        <?php echo html_entity_decode($noticia->body); ?>
+    </div>
 </div>
 
-<div class="row">
-        <div class="col-md-12 lineseparador separador10">
-        </div>
+<div class="col-md-12 column content-gris ">
+    <div class="col-md-4 column margen0">
+        <?php echo $noticia->category_id; ?>
+    </div>
+    <div class="col-md-8 column margen0 text-right text-news-zone">
+        <?php echo $noticia->tags[0]->name; ?>
+    </div>
 </div>
-<div class="col-md-12 text-right fondoazul separador10">
-    Más noticias
-</div>
+
+
+
+
