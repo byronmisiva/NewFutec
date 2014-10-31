f1d832f27600609078ee12638a99871d409b05e0
byronherrera <herrera.byron@gmail.com>
2014-10-31 15:41:23 -0500

    message

--- application/modules/templates/views/header.php
+++ application/modules/templates/views/header.php
@@ -1,27 +1,27 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="es">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="content-language" content="es">
-    <meta name="language" content="es">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta http-equiv="content-language" content="es"/>
+    <meta name="language" content="es"/>
 
-    <meta name="robots" content="follow,index,nocache">
+    <meta name="robots" content="follow,index,nocache"/>
 
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
 
     <!-- Fav and touch icons -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?=base_url('img/apple-touch-icon-144-precomposed.png')?>">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?=base_url('img/apple-touch-icon-114-precomposed.png')?>">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?=base_url('img/apple-touch-icon-72-precomposed.png')?>">
-    <link rel="apple-touch-icon-precomposed" href="<?=base_url('img/apple-touch-icon-57-precomposed.png')?>">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?=base_url('img/apple-touch-icon-144-precomposed.png')?>"/>
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?=base_url('img/apple-touch-icon-114-precomposed.png')?>"/>
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?=base_url('img/apple-touch-icon-72-precomposed.png')?>"/>
+    <link rel="apple-touch-icon-precomposed" href="<?=base_url('img/apple-touch-icon-57-precomposed.png')?>"/>
 
-    <link rel="apple-touch-icon" sizes="144x144" href="<?=base_url('img/apple-touch-icon-144-precomposed.png')?>">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?=base_url('img/apple-touch-icon-114-precomposed.png')?>">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?=base_url('img/apple-touch-icon-72-precomposed.png')?>">
-    <link rel="apple-touch-icon" href="<?=base_url('img/apple-touch-icon-57-precomposed.png')?>">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?=base_url('img/apple-touch-icon-144-precomposed.png')?>"/>
+    <link rel="apple-touch-icon" sizes="114x114" href="<?=base_url('img/apple-touch-icon-114-precomposed.png')?>"/>
+    <link rel="apple-touch-icon" sizes="72x72" href="<?=base_url('img/apple-touch-icon-72-precomposed.png')?>"/>
+    <link rel="apple-touch-icon" href="<?=base_url('img/apple-touch-icon-57-precomposed.png')?>"/>
 
 
     <link rel="icon" href="<?=base_url('assets/img/favicon.ico')?>">
