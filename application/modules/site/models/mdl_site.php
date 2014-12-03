ac1e4b93de4dc9c359626227b2db31253ae8ee5f
byronherrera <herrera.byron@gmail.com>
2014-12-03 18:17:15 -0500

    message

--- application/modules/site/models/mdl_site.php
+++ application/modules/site/models/mdl_site.php
@@ -1,25 +1,50 @@
 <?php
-class Mdl_site extends MY_Model{
-	
-	public $table_name = "sections";
-	public $primary_key = "id";	
-	public $joins;	
-	public $select_fields;	
-	public $total_rows;	
-	public $page_links;	
-	public $current_page;	
-	public $num_pages;	
-	public $optional_params;	
-	public $order_by;	
-	public $form_values = array();
 
-	public function __construct(){
-		parent::__construct();		
-	}
+class Mdl_site extends MY_Model
+{
 
-    function getNameSection($id){
-        $this->db->where( 'id',$id);
-        $this->db->from( 'sections');
+    public $table_name = "sections";
+    public $primary_key = "id";
+    public $joins;
+    public $select_fields;
+    public $total_rows;
+    public $page_links;
+    public $current_page;
+    public $num_pages;
+    public $optional_params;
+    public $order_by;
+    public $form_values = array();
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    function getNameSection($id)
+    {
+        $this->db->where('id', $id);
+        $this->db->from('sections');
+        return $this->db->get()->result();
+    }
+
+    function getNameTeam($name)
+    {
+        $this->db->where('name', $name);
+        $this->db->from('teams');
+        return $this->db->get()->result();
+    }
+
+    function getNameStadia($id)
+    {
+        $this->db->where('id', $id);
+        $this->db->from('stadia');
+        return $this->db->get()->result();
+    }
+
+    function getHistories($id)
+    {
+        $this->db->where('team_id', $id);
+        $this->db->from('histories');
         return $this->db->get()->result();
     }
     function getNameTeam($name){
