d89a63ae166753e35743dd556341c28fdf68b1d7
byronherrera <herrera.byron@gmail.com>
2014-12-16 14:35:17 -0500

    incluye tabla de revista

--- application/modules/site/controllers/site.php
+++ application/modules/site/controllers/site.php
@@ -392,6 +392,7 @@ class Site extends MY_Controller
             $dataTeam ['infoEquipo'] = $infoEquipo[0];
             $dataTeam ['infoJugadoresEquipo'] = $this->mdl_team->getJugadoresEquipo ($idEquipo) ;
 
+
             $infoEquipo = $this->team->getFichaEquipo($dataTeam);
             // fin informacion recupera
         } else {
