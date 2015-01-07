396963c6d500369bb6608de5513bf8ba18c7c865
byronherrera <herrera.byron@gmail.com>
2015-01-06 22:06:27 -0500

    incluye tabla de revista

--- application/modules/banners/views/fe_skin.php
+++ application/modules/banners/views/fe_skin.php
@@ -1,6 +1,6 @@
 
 <div id='fondo_brand'
-     style='position: fixed; _position: absolute; top: 144px; left: 50%; width: 1050px; height: 800px; margin-left: -900px;'>
+     style='position: fixed; _position: absolute; top: 164px; left: 50%; width: 1050px; height: 800px; margin-left: -900px;'>
     <!-- FE_SKIN -->
     <script type='text/javascript'>
         GA_googleFillSlot("FE_SKIN");
