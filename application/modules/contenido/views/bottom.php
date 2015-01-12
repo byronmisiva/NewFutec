3f3846c3b5b0eee89e18153fe28dbf3d62e64c34
byronherrera <herrera.byron@gmail.com>
2015-01-12 12:43:21 -0500

    incluye tabla de revista

--- application/modules/contenido/views/bottom.php
+++ application/modules/contenido/views/bottom.php
@@ -3,8 +3,16 @@
         2015 © Todos los derechos reservados.
     </div>
 
-    <div class="col-md-6 text-right">
-        <img src="<?php echo base_url(); ?>assets/img/logo_SMG.png" alt="logoSMG"/>
-        <a href="http://www.misiva.com.ec" target="_blank" alt="Misiva Web Site"><img src="<?php echo base_url(); ?>assets/img/logo_misiva.png" alt="logoMisiva"  /></a>
+    <div class="col-md-6  ">
+        <div class="col-md-6 col-xs-6 text-left margen0">
+            <img src="<?php echo base_url(); ?>assets/img/logo_SMG.png" alt="logoSMG"/>
+        </div>
+        <div class="col-md-6  col-xs-2 text-right margen0">
+
+            <a href="http://www.misiva.com.ec" target="_blank" alt="Misiva Web Site">
+                <img src="<?php echo base_url(); ?>assets/img/logo_misiva.png" alt="logoMisiva"/>
+            </a>
+
+        </div>
     </div>
 </div>
