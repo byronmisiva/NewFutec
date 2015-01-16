5de35ccd392596248c451f72256dd2c12283cdba
byronherrera <herrera.byron@gmail.com>
2015-01-16 10:15:53 -0500

    incluye tabla de revista

--- application/modules/noticias/views/noticiahomemini.php
+++ application/modules/noticias/views/noticiahomemini.php
@@ -8,7 +8,6 @@
     $link = base_url() . 'site/noticia/' . $this->noticias->_urlFriendly($story->title) . '/' . $story->id;
 }
 ?>
-
 <div class="margen0-xs clearfix news-detail">
     <div class="col-md-12  margen0 col-xs-2">
         <div class="noticia-img">
@@ -19,13 +18,13 @@
             </a>
         </div>
     </div>
-    <div class="col-md-12 margen0 column text-news-date hidden-xs">
+    <div class="col-md-12 margen0-noti column text-news-date hidden-xs">
         <a href="<?php echo $link ?>">
             <?php setlocale(LC_ALL, "es_ES");
             echo strftime("%d %B %Y", strtotime($story->created)); ?>
         </a>
     </div>
-    <div class="col-md-12 margen0 column col-xs-10 ">
+    <div class="col-md-12 margen0-noti column col-xs-10 ">
         <h2><a href="<?php echo $link ?>"><?php echo $story->title ?></a></h2>
     </div>
     <?php
@@ -36,17 +35,17 @@
         $bodyCortado = substr($str, 0, -(strlen($str) - strrpos($str, ' ')));
         echo '<a href="' . $link . '" class="sidebarlink">' . '</a>';
         ?>
-        <div class="col-md-12  margen0 col-xs-10  column mini-new-conten">
+        <div class="col-md-12  margen0-noti col-xs-10  column mini-new-conten">
             <a href="<?php echo $link ?>"> <?php echo $bodyCortado . "..." ?></a>
         </div>
     <?php
 
     } else {
         ?>
-        <div class="col-md-12 margen0 col-xs-10 column text-news-sub">
+        <div class="col-md-12 margen0-noti col-xs-10 column text-news-sub">
             <a href="<?php echo $link ?>"> <?php echo $story->subtitle ?></a>
         </div>
-        <div class="col-md-12  margen0 column mini-new-conten hidden-xs">
+        <div class="col-md-12  margen0-noti column mini-new-conten hidden-xs">
             <a href="<?php echo $link ?>"> <?php echo strip_tags($story->lead); ?></a>
         </div>
     <?php
@@ -56,7 +55,7 @@
     <div class="col-md-4 column margen0">
         Lecturas <?php echo $story->lecturas ?>
     </div>
-    <div class="col-md-8 column margen0 text-right text-news-zone">
+    <div class="col-md-8 column margen0-noti text-right text-news-zone">
         <?php echo $story->category ?>
     </div>
 </div>
