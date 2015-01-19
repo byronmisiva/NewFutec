74b3c180f40f34bd17cc72b05472d971631da847
byronherrera <herrera.byron@gmail.com>
2015-01-19 15:33:31 -0500

    incluye tabla de revista

--- application/modules/story/views/noticiaabierta.php
+++ application/modules/story/views/noticiaabierta.php
@@ -67,7 +67,34 @@
     <h4 class="panel-title">Comentarios </h4>
 </div>
 
-<div class="separador10 col-xs-12 col-md-12 center-block" data-href="<?php //echo $url?>">
-    <div class="fb-comments" data-href="<?php echo $link ?>" data-width="100%" data-numposts="5"
-         data-colorscheme="light"></div>
+<div class="separador10 col-xs-12 col-md-12 center-block" data-href="<?php echo $url?>">
+
+
+<div id="disqus_thread"></div>
+<script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_shortname = 'futbolecuador'; // required: replace example with your forum shortname
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 </div>
+
+
+<script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_shortname = 'futbolecuador'; // required: replace example with your forum shortname
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function () {
+        var s = document.createElement('script'); s.async = true;
+        s.type = 'text/javascript';
+        s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+    }());
+</script>
