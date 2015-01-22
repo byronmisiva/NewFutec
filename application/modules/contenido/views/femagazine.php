1000bba8c0b7f2a67c9c5b77f4db8d880d1332ba
byronherrera <herrera.byron@gmail.com>
2015-01-22 14:04:04 -0500

    incluye tabla de revista

--- application/modules/contenido/views/femagazine.php
+++ application/modules/contenido/views/femagazine.php
@@ -1,11 +1,215 @@
 <!--Tabla de posiciones-->
 <div class="col-md-12 separador10 margen0r">
-    <div class="panel-heading backcuadros">
-        <h4 class="panel-title">
-            Fe Magazine
-        </h4>
-    </div>
-    <div id='mod_femagazine' style='position:relative;width:100%; height:793px;' class="separador10-xs">
-        contenido 1
-    </div>
+<div class="panel-heading backcuadros">
+    <h4 class="panel-title">
+        Fe Magazine
+    </h4>
 </div>
+<div class="col-md-12 separador10  margen0 ">
+    <img src="<?= base_url('imagenes/femagazine/zonafe01.jpg'); ?>" class="img-responsive">
+
+</div>
+<div class="col-md-12 separador10  ">
+    <div class="col-md-6 separador10  ">
+        mensaje 1
+
+    </div>
+    <div class="col-md-6 separador10  ">
+        logos
+
+    </div>
+
+</div>
+<div class="col-md-12 text-left fondoazul separador10">
+    En esta edición
+</div>
+<div class="col-md-12  margen0 separador10">
+    <div class="col-md-12  margen0 ">
+        <div class="col-md-6  col-xs-12 margen0l separador10">
+            <div class="col-md-12  col-xs-12  clearfix borde">
+                <div class="col-md-4  col-xs-4 margen0 ">
+                    <img src="<?= base_url('imagenes/femagazine/seccion01.jpg'); ?>" class="img-circle img-responsive">
+                </div>
+                <div class="col-md-8  col-xs-8   ">
+                    <h2>Especial Emelec Campeón</h2>
+
+                    <p>El Club Sport Emelec se consagró campeón del torneo nacional 2014. Dos finales inolvidables en
+                        detalle, y disfruta de las galerías en el Monumental y el Capwell.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="clearfix col-md-6  col-xs-12 margen0l separador10">
+            <div class="col-md-12  col-xs-12  clearfix borde">
+                <div class="col-md-4  col-xs-4  margen0  ">
+                    <img src="<?= base_url('imagenes/femagazine/seccion03.jpg'); ?>" class="img-circle img-responsive">
+                </div>
+                <div class="col-md-8  col-xs-8   ">
+                    <h2>Directorio de Apps</h2>
+
+                    <p>Futbolecuador.com Magazine te presenta las mejores aplicaciones futboleras, para que lleves en tu
+                        dispositivo lo más destacado del deporte; en todo lugar y momento.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12  margen0 ">
+    <div class="clearfix col-md-6  col-xs-12 margen0l separador10  ">
+        <div class="col-md-12  col-xs-12  clearfix borde">
+            <div class="col-md-4  col-xs-4 margen0   ">
+                <img src="<?= base_url('imagenes/femagazine/seccion02.jpg'); ?>" class="img-circle img-responsive">
+            </div>
+            <div class="col-md-8  col-xs-8   ">
+                <h2>La de todoso</h2>
+
+                <p>La Seleccion Femenina marcó historia clasificando al Mundial de Canadá 2015. Resumen y análisis
+                    del repechaje ante Trinidad y Tobago. Además, el ranking FIFA de la Selección de mayores.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-6  col-xs-12 margen0l separador10">
+        <div class="col-md-12  col-xs-12  clearfix borde">
+            <div class="col-md-4  col-xs-4   margen0 ">
+                <img src="<?= base_url('imagenes/femagazine/seccion04.jpg'); ?>" class="img-circle img-responsive">
+            </div>
+            <div class="col-md-8  col-xs-8   ">
+                <h2>Especial Emelec Campeón</h2>
+
+                <p>El Club Sport Emelec se consagró campeón del torneo nacional 2014. Dos finales inolvidables en
+                    detalle, y disfruta de las galerías en el Monumental y el Capwell.
+                </p>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div class="col-md-12  margen0 ">
+    <div class="col-md-6  col-xs-12 margen0l separador10">
+        <div class="col-md-12  col-xs-12  clearfix borde">
+            <div class="col-md-4  col-xs-4   margen0 ">
+                <img src="<?= base_url('imagenes/femagazine/seccion05.jpg'); ?>" class="img-circle img-responsive">
+            </div>
+            <div class="col-md-8  col-xs-8   ">
+                <h2>Jóvenes promesas</h2>
+
+                <p>Los jóvenes talentos del fútbol ecuatoriano no paran de sorprender y sobresalir. Entérate de las
+                    futuras estrellas que brillan en nuestro país.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-6  col-xs-12 margen0l separador10">
+        <div class="col-md-12  col-xs-12  clearfix borde">
+            <div class="col-md-4  col-xs-4   margen0 ">
+                <img src="<?= base_url('imagenes/femagazine/seccion06.jpg'); ?>" class="img-circle img-responsive">
+            </div>
+            <div class="col-md-8  col-xs-8   ">
+                <h2>Rueda de prensa</h2>
+
+                <p>La opinión y reflexión de quienes hacen el fútbol ecuatoriano. Las frases de mayor importancia,
+                    que en su momento hicieron ruido en los medios de comunicación.
+                </p>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div class="col-md-12  margen0 ">
+    <div class="col-md-6  col-xs-12 margen0l separador10">
+        <div class="col-md-12  col-xs-12  clearfix borde">
+            <div class="col-md-4  col-xs-4   margen0 ">
+                <img src="<?= base_url('imagenes/femagazine/seccion07.jpg'); ?>" class="img-circle img-responsive">
+            </div>
+            <div class="col-md-8  col-xs-8   ">
+                <h2>Internacional</h2>
+
+                <p>El Real Madrid es el equipo mas ganador del último trimestre del año, Leo Messi rompe todos los
+                    records que se le pongan en frente. El arquero alemán Neuer va por el Balón de Oro.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-6  col-xs-12 margen0l separador10">
+        <div class="col-md-12  col-xs-12  clearfix borde">
+            <div class="col-md-4  col-xs-4   margen0 ">
+                <img src="<?= base_url('imagenes/femagazine/seccion09.jpg'); ?>" class="img-circle img-responsive">
+            </div>
+            <div class="col-md-8  col-xs-8   ">
+                <h2>90 minutos</h2>
+
+                <p>El ex dirigente de Liga de Quito, Esteban Paz, fue sancionado por un año. Conoce cómo se
+                    desarrolló el proceso, con sus antecedentes, posturas y conclusiones.
+                </p>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div class="col-md-12  margen0 ">
+    <div class="col-md-6  col-xs-12 margen0l separador10">
+        <div class="col-md-12  col-xs-12  clearfix borde">
+            <div class="col-md-4  col-xs-4   margen0 ">
+                <img src="<?= base_url('imagenes/femagazine/seccion11.jpg'); ?>" class="img-circle img-responsive">
+            </div>
+            <div class="col-md-8  col-xs-8   ">
+                <h2>Fuera de juego</h2>
+
+                <p>Diana Rivillas y su aliento de inicio a fin a Emelec, campeón ecuatoriano 2014. No te puedes
+                    perder de esta sesión de infarto.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-6  col-xs-12 margen0l separador10">
+        <div class="col-md-12  col-xs-12  clearfix borde">
+            <div class="col-md-4  col-xs-4   margen0 ">
+                <img src="<?= base_url('imagenes/femagazine/seccion10.jpg'); ?>" class="img-circle img-responsive">
+            </div>
+            <div class="col-md-8  col-xs-8   ">
+                <h2>Internacional</h2>
+
+                <p>El Real Madrid es el equipo mas ganador del último trimestre del año, Leo Messi rompe todos los
+                    records que se le pongan en frente. El arquero alemán Neuer va por el Balón de Oro.
+                </p>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div class="col-md-12  margen0 ">
+    <div class="col-md-6  col-xs-12 margen0l separador10">
+        <div class="col-md-12  col-xs-12  clearfix borde">
+            <div class="col-md-4  col-xs-4   margen0 ">
+                <img src="<?= base_url('imagenes/femagazine/seccion08.jpg'); ?>" class="img-circle img-responsive">
+            </div>
+            <div class="col-md-8  col-xs-8   ">
+                <h2>12 pasos</h2>
+
+                <p>Segundo Alejandro Castillo se confiesa y revela varios detalles de su vida y su carrera como
+                    futbolista. Uno de los históricos de La Tri.
+                </p>
+            </div>
+        </div>
+    </div>
+    </div>
+
+</div>
+
+<div class="col-md-12 separador10  ">
+    <div class="col-md-6 separador10  ">
+        mensaje 1
+
+    </div>
+    <div class="col-md-6 separador10  ">
+        logos
+
+    </div>
+
+</div>
+
+</div>
+
+
