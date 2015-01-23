4340bf4b7a6d4663521c3896fab823c7cab2903a
byronherrera <herrera.byron@gmail.com>
2015-01-23 14:13:27 -0500

    incluye tabla de revista

--- application/modules/noticias/views/noticiahomemini.php
+++ application/modules/noticias/views/noticiahomemini.php
@@ -9,22 +9,25 @@
 }
 ?>
 <div class="margen0-xs clearfix news-detail">
-    <div class="col-md-12 col-sm-12  margen0 col-xs-2">
+    <div class="col-md-12 col-xs-12 col-sm-12  margen0 col-xs-2">
         <div class="noticia-img">
             <a href="<?php echo $link ?>">
-                <img data-original="http://www.futbolecuador.com/<?php echo ($isMobile) ? $story->thumbh50:$story->thumb300; ?>"
-                     class="img-responsive lazy"
+                <img src="http://www.futbolecuador.com/<?php echo $story->thumbh50; ?>"
+                     class="img-responsive visible-xs-block"
+                     alt="<?php echo str_replace('"', '', "$story->title"); ?>">
+                <img data-original="http://www.futbolecuador.com/<?php echo $story->thumb300; ?>"
+                     class="img-responsive lazy hidden-xs"
                      alt="<?php echo str_replace('"', '', "$story->title"); ?>">
             </a>
         </div>
     </div>
-    <div class="col-md-12 margen0-noti column text-news-date hidden-xs">
+    <div class="col-md-12 col-xs-12 margen0-noti column text-news-date hidden-xs">
         <a href="<?php echo $link ?>">
             <?php setlocale(LC_ALL, "es_ES");
             echo ucfirst(strftime("%B %d %Y", strtotime($story->created))); ?>
         </a>
     </div>
-    <div class="col-md-12 col-sm-12 margen0-noti column col-xs-10 ">
+    <div class="col-md-12 col-xs-12 col-sm-12 margen0-noti column col-xs-10 ">
         <h2><a href="<?php echo $link ?>"><?php echo $story->title ?></a></h2>
     </div>
     <?php
@@ -35,23 +38,23 @@
         $bodyCortado = substr($str, 0, -(strlen($str) - strrpos($str, ' ')));
         echo '<a href="' . $link . '" class="sidebarlink">' . '</a>';
         ?>
-        <div class="col-md-12  col-sm-12  margen0-noti col-xs-10  column mini-new-conten">
+        <div class="col-md-12 col-xs-12  col-sm-12  margen0-noti col-xs-10  column mini-new-conten">
             <a href="<?php echo $link ?>"> <?php echo $bodyCortado . "..." ?></a>
         </div>
     <?php
 
     } else {
         ?>
-        <div class="col-md-12 col-sm-12 margen0-noti col-xs-10 column text-news-sub">
+        <div class="col-md-12 col-xs-12 col-sm-12 margen0-noti col-xs-10 column text-news-sub">
             <a href="<?php echo $link ?>"> <?php echo $story->subtitle ?></a>
         </div>
-        <div class="col-md-12  col-sm-12 margen0-noti column mini-new-conten hidden-xs">
+        <div class="col-md-12 col-xs-12  col-sm-12 margen0-noti column mini-new-conten hidden-xs">
             <a href="<?php echo $link ?>"> <?php echo strip_tags($story->lead); ?></a>
         </div>
     <?php
     }?>
 </div>
-<div class="col-md-12 col-sm-12 column content-gris hidden-xs">
+<div class="col-md-12 col-xs-12 col-sm-12 column content-gris hidden-xs">
     <div class="col-md-4 col-sm-4 column margen0">
         Lecturas <?php echo $story->lecturas ?>
     </div>
