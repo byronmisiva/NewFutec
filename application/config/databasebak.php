82e4676b1b0af47ef94be88f655aa43ce02d0387
byronherrera <herrera.byron@gmail.com>
2015-01-26 10:53:11 -0500

    incluye tabla de revista

---
+++ application/config/databasebak.php
@@ -0,0 +1,32 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+ 
+| the active record class
+*/
+
+$active_group = 'default';
+$active_record = TRUE;
+
+$db['default']['hostname'] = "69.64.67.206";
+$db['default']['username'] = "futec";
+$db['default']['password'] = "feadmin06";
+$db['default']['database'] = "fe2008";
+
+$db['default']['hostname'] = "127.0.0.1";
+$db['default']['username'] = "root";
+$db['default']['password'] = "";
+$db['default']['database'] = "fe2008";
+
+$db['default']['dbdriver'] = "mysql";
+$db['default']['dbprefix'] = "";
+$db['default']['pconnect'] = TRUE;
+$db['default']['db_debug'] = TRUE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = "";
+$db['default']['char_set'] = "utf8";
+$db['default']['dbcollat'] = "utf8_general_ci";
+
+
+
+/* End of file database.php */
+/* Location: ./application/config/database.php */
