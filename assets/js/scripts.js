68b6da16c2362dbd233bc2146155574ba2ef47c0
byronherrera <herrera.byron@gmail.com>
2015-02-02 12:11:27 -0500

    incluye tabla de revista

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,16 +2,16 @@ var masnoticas = "";
 
 //inicio funciones splash
 function cargarSplash(){
-    if($('div-gpt-ad-1383593884981-1').getStyle('display') !== "none"){
-        $('darkLayer').show();
-        $('FE_LOADING').show();
+    if(!$('#div-gpt-ad-1383593884981-1').is(":visible")){
+        $('#darkLayer').show();
+        $('#FE_LOADING').show();
         setTimeout(cleanBlackLayer,15000);
     }
 };
 
 var cleanBlackLayer = function(){
-    $('FE_LOADING').hide();
-    $('darkLayer').hide();
+    $('#FE_LOADING').hide();
+    $('#darkLayer').hide();
 };
 
 
