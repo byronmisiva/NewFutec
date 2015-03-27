1756a4dd7c8bea7f42c70aaf9a4e27bda1149436
byronherrera <herrera.byron@gmail.com>
2015-03-27 12:18:05 -0500

    e

--- application/modules/banners/views/fe_smart_bottom.php
+++ application/modules/banners/views/fe_smart_bottom.php
@@ -1,5 +1,5 @@
 <!-- FE_SMART_BOTTOM -->
-<div id='div-gpt-ad-1383593619381-2' style='width:320px; height:auto;margin:0 auto;'>
+<div id='div-gpt-ad-1383593619381-2' class="respiframe">
     <script type='text/javascript'>
         googletag.cmd.push(function() { googletag.display('div-gpt-ad-1383593619381-2'); });
     </script>
