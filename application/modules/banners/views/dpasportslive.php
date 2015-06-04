531b55c10910ac9eb052aeafbfb013ba9eee60fa
byronherrera <herrera.byron@gmail.com>
2015-06-04 17:26:04 -0500

    e

--- application/modules/banners/views/dpasportslive.php
+++ application/modules/banners/views/dpasportslive.php
@@ -1,37 +1,44 @@
-<div id="dpasportslive" class="banner300x250 hidden-xs">
-    <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="300" height="83" id="marcador" align="middle">
-        <param name="movie" value="<?php echo base_url('imagenes/febanners/marcador.swf') ?>" />
-        <param name="quality" value="high" />
-        <param name="bgcolor" value="#ffffff" />
-        <param name="play" value="true" />
-        <param name="loop" value="true" />
-        <param name="wmode" value="window" />
-        <param name="scale" value="showall" />
-        <param name="menu" value="true" />
-        <param name="devicefont" value="false" />
-        <param name="salign" value="" />
-        <param name="allowScriptAccess" value="sameDomain" />
-        <!--[if !IE]>-->
-        <object type="application/x-shockwave-flash" data="<?php echo base_url('imagenes/febanners/marcador.swf') ?>" width="300" height="83">
-            <param name="movie" value="marcador.swf" />
-            <param name="quality" value="high" />
-            <param name="bgcolor" value="#ffffff" />
-            <param name="play" value="true" />
-            <param name="loop" value="true" />
-            <param name="wmode" value="window" />
-            <param name="scale" value="showall" />
-            <param name="menu" value="true" />
-            <param name="devicefont" value="false" />
-            <param name="salign" value="" />
-            <param name="allowScriptAccess" value="sameDomain" />
-            <!--<![endif]-->
-            <a href="http://www.adobe.com/go/getflash">
-                <img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Obtener Adobe Flash Player" />
-            </a>
+<div id="dpasportslive" class=" hidden-xs">
+    <div id="dpasportsliveover"> </div>
+
+    <div id="dpasportslive" class="banner300x250 hidden-xs">
+        <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="300" height="83" id="marcador"
+                align="middle">
+            <param name="movie" value="<?php echo base_url('imagenes/febanners/marcador.swf') ?>"/>
+            <param name="quality" value="high"/>
+            <param name="bgcolor" value="#ffffff"/>
+            <param name="play" value="true"/>
+            <param name="loop" value="true"/>
+            <param name="wmode" value="window"/>
+            <param name="scale" value="showall"/>
+            <param name="menu" value="true"/>
+            <param name="devicefont" value="false"/>
+            <param name="salign" value=""/>
+            <param name="allowScriptAccess" value="sameDomain"/>
             <!--[if !IE]>-->
+            <object type="application/x-shockwave-flash"
+                    data="<?php echo base_url('imagenes/febanners/marcador.swf') ?>" width="300" height="83">
+                <param name="movie" value="marcador.swf"/>
+                <param name="quality" value="high"/>
+                <param name="bgcolor" value="#ffffff"/>
+                <param name="play" value="true"/>
+                <param name="loop" value="true"/>
+                <param name="wmode" value="window"/>
+                <param name="scale" value="showall"/>
+                <param name="menu" value="true"/>
+                <param name="devicefont" value="false"/>
+                <param name="salign" value=""/>
+                <param name="allowScriptAccess" value="sameDomain"/>
+                <!--<![endif]-->
+                <a href="http://www.adobe.com/go/getflash">
+                    <img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif"
+                         alt="Obtener Adobe Flash Player"/>
+                </a>
+                <!--[if !IE]>-->
+            </object>
+            <!--<![endif]-->
         </object>
-        <!--<![endif]-->
-    </object>
+    </div>
 </div>
 <!--dpasportslive-->
 <!--<div id="dpasportslive" class="banner300x250 hidden-xs">
@@ -41,13 +48,12 @@
             src="http://sportslive-feed.com.s3.amazonaws.com/futbolecuador/html/indexSAScoreboard.html"></iframe>
 </div>-->
 <!--dpasportslivepopup-->
-<!--<div class="flotante hidden-xs">
+<div class="flotante hidden-xscc">
     <div id="closeBannerdpa">
-        <img src="<?php /*echo base_url('assets/img/close_banner.png') */?>" width="81"
+        <img src="<?php echo base_url('assets/img/close_banner.png') ?>" width="81"
              height="20">
     </div>
     <div class="contenedor">
 
     </div>
 </div>
--->
