f6b1b2793bb153e6fd4362e32d0ca43a9f351273
byronherrera <herrera.byron@gmail.com>
2015-06-04 17:49:00 -0500

    e

--- application/modules/banners/views/dpasportsliveiframe.php
+++ application/modules/banners/views/dpasportsliveiframe.php
@@ -1,4 +1,4 @@
-2<!--Tabla de Posiciones-->
+<!--Tabla de Posiciones-->
 <div class="col-md-12 separador10-xs margen0r">
     <div class="panel-heading backcuadros">
         <h4 class="panel-title">
