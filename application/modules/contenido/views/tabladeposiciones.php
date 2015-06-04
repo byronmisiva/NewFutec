34b67cda2451422dfbead0f4dd3ea15269b1f4cd
byronherrera <herrera.byron@gmail.com>
2015-06-03 20:17:28 -0500

    e

--- application/modules/contenido/views/tabladeposiciones.php
+++ application/modules/contenido/views/tabladeposiciones.php
@@ -4,30 +4,22 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta http-equiv="content-language" content="es"/>
     <meta name="robots" content="follow,index,nocache"/>
-    <link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
-    <link href="<?php echo base_url('assets/css/style.css') ?>" rel="stylesheet">
-    <link href="<?php echo base_url('assets/css/revista.css') ?>" rel="stylesheet">
-    <script type="text/javascript" src="<?php echo base_url() ?>assets/js/jquery.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>
+    <link href="<?php echo base_url('assets/css/bootstrap.min.css?ran=' . rand(1, 1000)) ?>" rel="stylesheet">
+    <link href="<?php echo base_url('assets/css/style.css?ran=' . rand(1, 1000)) ?>" rel="stylesheet">
+    <link href="<?php echo base_url('assets/css/revista.css?ran=' . rand(1, 1000)) ?>" rel="stylesheet">
+    <script type="text/javascript" src="<?php echo base_url('assets/js/jquery.min.js' . rand(1, 1000)) ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets/js/jquery.min.js' . rand(1, 1000)) ?>assets/js/bootstrap.min.js"></script>
     <link href='http://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
 
     <?php
     if ($verMobile == "1") {
         ?>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
-        <script type="text/javascript" async defer
-                src="https://apis.google.com/js/platform.js?publisherid=109198533032839133083">
-        </script>
+
     <?php
     } else {
         if ($verMobile == "2") {
             ?>
-            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
-        <?php
-        } else {
-            ?>
-            <link rel="chrome-webstore-item"
-                  href="https://chrome.google.com/webstore/detail/cjkoikfgconobaeikllfnkpnjihcfnil">
             <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
         <?php
         }
