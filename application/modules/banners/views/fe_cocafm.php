0494f36efd7d6d92d91847c1fa7377586c6923bf
Byron <byronherrera@hotmail.com>
2015-09-30 16:13:55 -0500

    e

--- application/modules/banners/views/fe_cocafm.php
+++ application/modules/banners/views/fe_cocafm.php
@@ -1,18 +1,4 @@
 <div style="width: 240px; margin:auto;" class="hidden-xs hidden-sm">
-<!--    <object id="ccfmplayerec1" width="180" height="90”><param name=" movie
-    " value="http://www.imusicaradios.com/ccfmv2/player/?ct=ec&amp;type=horizontal&amp;embed&amp;resize&amp;width=180&amp;height=90&amp;volumestart=0&amp;plain&amp;config=http://www.imusicaradios.com/ccfmv2/ec/jsonconfig.txt&amp;typeoffplayer=ccfm-horizontal-player-assets&amp;build=6.0.3"
-    />
-    <param name="allowFullScreen" value="true"/>
-    <param name="wmode" value="transparent"/>
-    <param name="allowscriptaccess" value="always"/>
-    <embed
-        src="http://www.imusicaradios.com/ccfmv2/player/?ct=ec&amp;type=horizontal&amp;embed&amp;resize&amp;width=180&amp;height=90&amp;volumestart=0&amp;plain&amp;config=http://www.imusicaradios.com/ccfmv2/ec/jsonconfig.txt&amp;typeoffplayer=ccfm-horizontal-player-assets&amp;build=6.0.3"
-        name="ccfmplayerec" width="180" height="90" allowscriptaccess="always" wmode="transparent"
-        type="application/x-shockwave-flash" allowfullscreen="true"></embed>
-    </object>-->
-
     <script type="text/javascript" src="https://www.imusicaradios.com.br/go_ccfm/ccfm_embed.min.js" onload="CocaColaEmbed('ec','true',0,'red','Elfutbolero')"></script>
     <iframe id="ccfmPlayer" style="width: 240px; height:90px;"></iframe>
 </div>
-
-
