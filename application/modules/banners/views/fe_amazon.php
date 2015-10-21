769234ec742b4a31aae18884c89d5cd8e2431296
Byron <byronherrera@hotmail.com>
2015-10-21 18:06:34 -0500

    e

--- application/modules/banners/views/fe_amazon.php
+++ application/modules/banners/views/fe_amazon.php
@@ -1,5 +1,5 @@
 <!-- /1022247/Amazon_associates -->
-<div id='div-gpt-ad-1445466832316-0' style='height:370px; width:665px;' class="hidden-xs ">
+<div id='div-gpt-ad-1445466832316-0' style='height:370px; width:665px;' class="hidden-xs hidden ">
     <script type='text/javascript'>
         googletag.cmd.push(function() { googletag.display('div-gpt-ad-1445466832316-0'); });
     </script>
