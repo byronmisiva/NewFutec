95730e996ec739800e2b4178288cff256b1b17f8
Byron <byronherrera@hotmail.com>
2015-12-23 10:37:58 -0500

    e

--- application/modules/banners/views/fe_skyscraper_de.php
+++ application/modules/banners/views/fe_skyscraper_de.php
@@ -1,7 +1,8 @@
-<div class="skyscraper_de" style="position: fixed;
-    top: 200px;
-    left: 50%;
-    width: 1050px;
-    height: 600px;
-    margin-left: 500px;
-    "><img alt="160x600" src="http://placehold.it/160x600"></div>
+<div class="skyscraper_de" style="position: fixed; top: 200px; left: 50%; width: 1050px; height: 600px; margin-left: 500px;">
+    <!-- /1022247/FE_SKYSCRAPER_DE -->
+    <div id='div-gpt-ad-1450734059657-0' style='height:600px; width:160px;'>
+        <script type='text/javascript'>
+            googletag.cmd.push(function() { googletag.display('div-gpt-ad-1450734059657-0'); });
+        </script>
+    </div>
+</div>
