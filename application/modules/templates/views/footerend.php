1bb6d4f2230716c40e678496bca6676b7bc9838a
mivida1985 <jortiz@misiva.com.ec>
2016-04-12 15:09:47 -0500

    actualizacion de archo, el cual deja de cargar el script de g13 el mismo solo se desplegara en la noticia abierta

--- application/modules/templates/views/footerend.php
+++ application/modules/templates/views/footerend.php
@@ -29,24 +29,7 @@
 
 <script src="<?php echo base_url('assets/js/smartbanner/jquery.smartbanner.js') ?>"></script>
 
-<!-- Codigo CAG inicio -->
-    <script data-cfasync="false" type="text/javascript">
-      (function (document, window) {
-                var c = document.createElement("script");
-                c.type = "text/javascript";
-                c.async = !0;
-                c.src = "//hipersushiads.com/scripts/57af6c29eb5a32f5f09b20ff22b3e0b9.min.js?20160329=" + Math.floor((new Date).getTime());
-                var a = !1;
-                try {
-                    a = parent.document.getElementsByTagName("script")[0] || document.getElementsByTagName("script")[0];
-                } catch (e) {
-                    a = !1;
-                }
-                a || ( a = document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]);
-                a.parentNode.insertBefore(c, a);
-            })(document, window);
-      </script>
-<!-- Codigo CAG fin -->
+
 
 <script type="text/javascript">
     $(document).ready(function () {
