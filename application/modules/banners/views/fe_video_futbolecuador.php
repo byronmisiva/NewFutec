deb8ebdacf4a084440b0e866a2001c55cf09be20
mivida1985 <jortiz@misiva.com.ec>
2016-04-12 15:52:57 -0500

    actualizacion video banner futbolecuador

--- application/modules/banners/views/fe_video_futbolecuador.php
+++ application/modules/banners/views/fe_video_futbolecuador.php
@@ -3,7 +3,7 @@ $idtipo = $this->uri->segment(2);
 $tipo = array("noticia", "nuestrosembajadores", "lavoz", "zonafe", "equipo", "masleido");
 if (in_array($idtipo, $tipo)) { ?>
 <style>
-.cerrar{position:absolute;right: 0;top:0;width:100px;height: 35px;cursor: pointer;}
+.cerrar{position:absolute;right: 0;top:0;width:100px;height: 35px;cursor: pointer;z-index:100;}
 </style>
  <!-- /1022247/NEW_FE_Video_VAST -->
 <div id='div-gpt-ad-1457102356654-0' style='height:auto; width:670px;min-height: 10px;float: left;'>
@@ -14,11 +14,15 @@ if (in_array($idtipo, $tipo)) { ?>
 </div>
 
 <script>
+$(document).ready(function(){
 	$(".cerrar").click(function(){
 		$("#div-gpt-ad-1457102356654-0").toggle();
 	});
+	
 	setTimeout(function(){ 
 		$("#div-gpt-ad-1457102356654-0").toggle();;
 	}, 400000);
+});
+	
 </script>
 <?php }?>
