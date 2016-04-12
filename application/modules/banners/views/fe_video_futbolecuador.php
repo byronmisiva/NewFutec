b1356371bfea945ec1587024d34882eac756a32d
mivida1985 <jortiz@misiva.com.ec>
2016-04-12 15:58:39 -0500

    actualizacion de codigo

--- application/modules/banners/views/fe_video_futbolecuador.php
+++ application/modules/banners/views/fe_video_futbolecuador.php
@@ -3,14 +3,15 @@ $idtipo = $this->uri->segment(2);
 $tipo = array("noticia", "nuestrosembajadores", "lavoz", "zonafe", "equipo", "masleido");
 if (in_array($idtipo, $tipo)) { ?>
 <style>
-.cerrar{position:absolute;right: 0;top:0;width:100px;height: 35px;cursor: pointer;z-index:100;}
+.cerrar{float:right;z-index:100;height: 45px;width: 100%;border:1px solid red;}
 </style>
  <!-- /1022247/NEW_FE_Video_VAST -->
 <div id='div-gpt-ad-1457102356654-0' style='height:auto; width:670px;min-height: 10px;float: left;'>
+	<div class="cerrar">CERRAR</div>
 	<script type='text/javascript'>
 		googletag.cmd.push(function() { googletag.display('div-gpt-ad-1457102356654-0'); });
 	</script>
-	<div class="cerrar">CERRAR</div>
+	
 </div>
 
 <script>
@@ -19,10 +20,10 @@ $(document).ready(function(){
 		$("#div-gpt-ad-1457102356654-0").toggle();
 	});
 	
-	setTimeout(function(){ 
-		$("#div-gpt-ad-1457102356654-0").toggle();;
-	}, 400000);
+
 });
-	
+setTimeout(function(){ 
+	$("#div-gpt-ad-1457102356654-0").toggle();;
+}, 400000);	
 </script>
 <?php }?>
