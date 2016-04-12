ee8dc2fd30bf480204b2dea8cf0d838617f67cde
mivida1985 <jortiz@misiva.com.ec>
2016-04-12 15:07:00 -0500

    actualizacion noticia relacionada

--- application/modules/noticias/views/noticiarelacionada.php
+++ application/modules/noticias/views/noticiarelacionada.php
@@ -4,20 +4,14 @@
     } else {  }
 } else {
     $link = base_url() . 'site/noticia/' . $this->noticias->_urlFriendly($story->title) . '/' . $story->id;
-}
-?>
-
-	<a  href="<?php echo $link ?>" onclick="ga('send', 'event', 'Relacionadas', 'click', 'noticia');">
-		<div class="col-xs-4 col-md-12 img-relacionada" >
-			<img src="http://www.futbolecuador.com/<?php echo $story->thumbh120; ?>"
-			     alt="<?php echo str_replace('"', '', "$story->title"); ?>"
-			     title="<?php echo str_replace('"', '', "$story->title"); ?>"			     
-			 	/>
-		 </div>
-		 <div class="col-xs-8 col-md-12 titular-reciente">
-				 <?php echo $story->title ?>
-		 </div>
-		 </a>
+}?>
+<a  href="<?php echo $link ?>" onclick="ga('send', 'event', 'Relacionadas', 'click', 'noticia');">
+<div class="col-xs-4 col-md-12 img-relacionada" >
+<img src="http://www.futbolecuador.com/<?php echo $story->thumbh120; ?>"
+    alt="<?php echo str_replace('"', '', "$story->title"); ?>" title="<?php echo str_replace('"', '', "$story->title"); ?>" />
+</div>
+<div class="col-xs-8 col-md-12 titular-reciente"><?php echo $story->title ?></div>
+</a>
 
 
  
