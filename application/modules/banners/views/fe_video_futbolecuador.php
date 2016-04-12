1979e1dc1b1576fb71b4183bb8ca4d04b4a2a8dd
mivida1985 <jortiz@misiva.com.ec>
2016-04-12 16:10:49 -0500

    alineacion de texto a la derecha

--- application/modules/banners/views/fe_video_futbolecuador.php
+++ application/modules/banners/views/fe_video_futbolecuador.php
@@ -3,7 +3,7 @@ $idtipo = $this->uri->segment(2);
 $tipo = array("noticia", "nuestrosembajadores", "lavoz", "zonafe", "equipo", "masleido");
 if (in_array($idtipo, $tipo)) { ?>
 <style>
-.cerrar{float:right;z-index:100;height:auto;width: 100%;text-align: center;padding-right: 15px;cursor:pointer;}
+.cerrar{float:right;z-index:100;height:auto;width: 100%;text-align: right;padding-right: 15px;cursor:pointer;}
 </style>
  <!-- /1022247/NEW_FE_Video_VAST -->
 <div id='div-gpt-ad-1457102356654-0' style='height:auto; width:670px;min-height: 10px;float: left;'>
