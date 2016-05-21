108750804199491f68d79e761a69dd6f83134dc5
mivida1985 <jortiz@misiva.com.ec>
2016-05-20 19:28:49 -0500

    Actualizacion de views en templates

---
+++ application/modules/templates/views/footerendbanner.php
@@ -0,0 +1,20 @@
+<script type="text/javascript">_atrk_opts = {atrk_acct: "A9Dnf1aUOO00Gi", domain: "futbolecuador.com"};
+    atrk();</script>
+<noscript>
+    <img src="https://d5nxst8fruw4z.cloudfront.net/atrk.gif?account=A9Dnf1aUOO00Gi" style="display: none" height="1"
+         width="1" alt=""/>
+</noscript>
+<!-- End Alexa Certify Javascript -->
+
+<div style='display: none;'>
+    <a href="http://www.alexa.com/siteinfo/www.futbolecuador.com">
+        <script type='text/javascript' src='http://xslt.alexa.com/site_stats/js/t/a?url=www.futbolecuador.com'></script>
+    </a> <a href="http://www.alexa.com/siteinfo/www.futbolecuador.com?p=rwidget#reviews"><img
+            src='http://www.alexa.com/images/widgets/blue/light/v1-125x60.png'
+            alt='Review www.futbolecuador.com on alexa.com'/> </a>
+</div>
+
+
+
+</body>
+</html>
---
+++ application/modules/templates/views/headerbanner.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta http-equiv="content-language" content="es"/>
+    <script type='text/javascript'>
+  var googletag = googletag || {};
+  googletag.cmd = googletag.cmd || [];
+  (function() {
+    var gads = document.createElement('script');
+    gads.async = true;
+    gads.type = 'text/javascript';
+    var useSSL = 'https:' == document.location.protocol;
+    gads.src = (useSSL ? 'https:' : 'http:') +
+      '//www.googletagservices.com/tag/js/gpt.js';
+    var node = document.getElementsByTagName('script')[0];
+    node.parentNode.insertBefore(gads, node);
+  })();
+</script>
+<script type='text/javascript'>
+  googletag.cmd.push(function() {
+    googletag.defineSlot('/1022247/Facebook_article', [320, 50], 'div-gpt-ad-1460670309337-0').addService(googletag.pubads());
+    googletag.pubads().enableSingleRequest();
+    googletag.enableServices();
+  });
+</script>
+</head>
+<body>
+
+
+
+
+
