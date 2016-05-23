79a412cb4b052a2a94574dd7fb10d75860804c20
mivida1985 <jortiz@misiva.com.ec>
2016-05-23 18:55:38 -0500

    actualizacion

--- application/modules/templates/views/footerend.php
+++ application/modules/templates/views/footerend.php
@@ -29,6 +29,12 @@
 <div class="pbl-union" >
     <div id="pbl-coke">
     	<div id="Stage" class="EDGE-28231535"></div>
+    	<!-- /1022247/FE_NEW_UNIONLAYER -->
+		<div id='div-gpt-ad-1464046739579-0' style='height: 600px;width: 800px;position: absolute;top: 0;left: 264px;'>
+		<script type='text/javascript'>
+		googletag.cmd.push(function() { googletag.display('div-gpt-ad-1464046739579-0'); });
+		</script>
+		</div>
     </div>
 </div>
 <script type="text/javascript">
