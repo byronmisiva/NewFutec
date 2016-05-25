1aed9061a25da266d1f3d13b155292b78933dd5e
mivida1985 <jortiz@misiva.com.ec>
2016-05-25 16:47:39 -0500

    actualizacion codigo

--- application/modules/banners/views/fe_tap__tap_home.php
+++ application/modules/banners/views/fe_tap__tap_home.php
@@ -1,16 +1,15 @@
-<script src="http://redirect-sonata.taptapnetworks.com/pyotr/lib/sonata?v=1.8"
-type="text/javascript"></script>
+<script src="http://redirect-sonata.taptapnetworks.com/pyotr/lib/sonata?v=1.8" type="text/javascript"></script>
 <script type="text/javascript">
-adRequest({
-z:'573cab0efc5934cd0160ea0d',
-p:'bottom',
-t:10,
-tg:'_blank',
-l:'http://d284k5tgq8ijip.cloudfront.net/lib/sonata/v.1.7/img/loader.gif',
-g: true,
-ads:0,
-noads:0,
-types:[],
-adCtrl:[]
-});
+	adRequest({
+		z:'573cab0efc5934cd0160ea0d',
+		p:'bottom',
+		t:10,
+		tg:'_blank',
+		l:'http://d284k5tgq8ijip.cloudfront.net/lib/sonata/v.1.7/img/loader.gif',
+		g: true,
+		ads:0,
+		noads:0,
+		types:[],
+		adCtrl:[]
+	});
 </script>
