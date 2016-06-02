a9b49aee4e12d75cace68930ba91dd925cb1746e
mivida1985 <jortiz@misiva.com.ec>
2016-06-02 16:30:40 -0500

    actualizacion

---
+++ application/modules/banners/views/fe_square_copa.php
@@ -0,0 +1,6 @@
+<!-- /1022247/FE_NEW_SQUAREBANNER_AUSPICIO_COPA_AMERICA -->
+<div id='div-gpt-ad-1464883629839-0' style='height:250px; width:300px;'>
+<script type='text/javascript'>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1464883629839-0'); });
+</script>
+</div>
