0474580345af2dfeaf8ec2bc654d992547f8ec94
mivida1985 <jortiz@misiva.com.ec>
2016-06-03 13:41:39 -0500

    actualizacion codigo

--- application/modules/banners/views/fe_cinta_copa.php
+++ application/modules/banners/views/fe_cinta_copa.php
@@ -1,11 +1,8 @@
-<!-- /1022247/FE_NEW_CINTA_AUSPICIO_COPA_AMERICA -->
-<div id='div-gpt-ad-1464883876542-0' style='height:50px; width:980px;'>
+<div id="div-gpt-ad-1464883876542-0" style='height:50px; width:980px;' class="hidden-xs hidden-sm ">
 	<script type='text/javascript'>
 		googletag.cmd.push(function() { googletag.display('div-gpt-ad-1464883876542-0'); });
 	</script>
 </div>
-
-
 <script type="text/javascript">
     function see_fold2(estado) {
         if (estado == 'on') {
