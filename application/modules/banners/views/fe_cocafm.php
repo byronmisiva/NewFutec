421fd0025a1dd8630c9d4e2c0c3bfbd1278dbe3e
mivida1985 <jortiz@misiva.com.ec>
2016-06-03 20:50:34 -0500

    actualizacion de codigo

--- application/modules/banners/views/fe_cocafm.php
+++ application/modules/banners/views/fe_cocafm.php
@@ -1,4 +1,5 @@
 <div style="width: 240px; margin:auto;" class="hidden-xs hidden-sm">
-    <script type="text/javascript" src="https://www.imusicaradios.com.br/go_ccfm/ccfm_embed.js" onload="CocaColaEmbed('ec','true',2,'red','Futbolecuador')"></script>
+	<!-- <iframe src="http://www.misiva.com.ec/banners/html5/html_copa_america/copa_boton/" frameborder="0" scrolling="no" style="width: 240px; height:90px;"></iframe>-->-->
     <iframe id="ccfmPlayer" style="width: 240px; height:90px;"></iframe>
+    <script type="text/javascript" src="https://www.imusicaradios.com.br/go_ccfm/ccfm_embed.js" onload="CocaColaEmbed('ec','true',2,'red','Futbolecuador')"></script>
 </div>
