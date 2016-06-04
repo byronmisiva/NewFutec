4702811a25dbcf38b72bd71d96438e78e84a96b2
mivida1985 <jortiz@misiva.com.ec>
2016-06-03 20:47:33 -0500

    actualizacion codigo

--- application/modules/scoreboards/views/tablaposicionessimple.php
+++ application/modules/scoreboards/views/tablaposicionessimple.php
@@ -1,5 +1,5 @@
 <div class="col-md-12 col-xs-12 separador10-xs-bot margen0r ">
-    <div class="panel-heading backcuadros">
+    <div class="panel-heading backcuadros auspicio side-tabla">
         <h4 class="panel-title">
             Tabla de Posiciones
         </h4>
