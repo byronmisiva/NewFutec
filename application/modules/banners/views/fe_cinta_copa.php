7b7e2a00b0d791dd5edbc1b1067d24ee72c28441
mivida1985 <jortiz@misiva.com.ec>
2016-06-03 20:50:54 -0500

    actualizacion

--- application/modules/banners/views/fe_cinta_copa.php
+++ application/modules/banners/views/fe_cinta_copa.php
@@ -1,4 +1,5 @@
-<div id="div-gpt-ad-1464883876542-0" style='height:50px; width:980px;' class="hidden-xs hidden-sm ">
+<div id="div-gpt-ad-1464883876542-0" 
+     style='height:50px; width:980px;' class="hidden-xs hidden-sm ">
 	<script type='text/javascript'>
 		googletag.cmd.push(function() { googletag.display('div-gpt-ad-1464883876542-0'); });
 	</script>
