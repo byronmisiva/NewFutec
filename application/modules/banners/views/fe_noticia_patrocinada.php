b2c732cadbda6c41716cd0b058e7adfece54039c
mivida1985 <jortiz@misiva.com.ec>
2016-06-06 16:14:41 -0500

    actualizacion de codigo

---
+++ application/modules/banners/views/fe_noticia_patrocinada.php
@@ -0,0 +1,6 @@
+<!-- /1022247/FE_NOTICIA_PATROCINADA -->
+<div id='div-gpt-ad-1465241512633-0' style='height:250px; width:300px;'>
+<script type='text/javascript'>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465241512633-0'); });
+</script>
+</div>
