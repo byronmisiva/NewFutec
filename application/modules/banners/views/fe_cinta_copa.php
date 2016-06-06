28a1a4cfbe6256c8852dc37e8f063ffeb361a721
mivida1985 <jortiz@misiva.com.ec>
2016-06-06 16:07:39 -0500

    actualizacion codigo

--- application/modules/banners/views/fe_cinta_copa.php
+++ application/modules/banners/views/fe_cinta_copa.php
@@ -5,13 +5,28 @@
 	</script>
 </div>
 <script type="text/javascript">
-    function see_fold2(estado) {
+     function see_fold2(estado) {
         if (estado == 'on') {
             //agrandamos a 200 el banner
             $('#div-gpt-ad-1464883876542-0').css("height", "200px");
+           var contenedorCinta= $("#div-gpt-ad-1464883876542-0");    
+           var iframeCinta= contenedorCinta[0].lastChild["childNodes"][0];
+           $(iframeCinta).attr("height","200px");
+            
         } else {
             //disminuimos a 50
             $('#div-gpt-ad-1464883876542-0').css("height", "50px");
+            var contenedorCinta = $("#div-gpt-ad-1464883876542-0");    
+            var iframeCinta = contenedorCinta[0].lastChild["childNodes"][0];
+            $(iframeCinta).attr("height","50px");
         }
     }
+
+     
+     var contenedorheader= $("#div-gpt-ad-1383593619381-0");    
+     var iframeHeader=contenedorheader[0].lastChild["childNodes"][0];
+     $(iframeHeader).attr("height","50px");
+     
+
+     
 </script>
