1fc95b5b4d22340f317af36ff5e90914ebf5665a
mivida1985 <jortiz@misiva.com.ec>
2016-06-06 20:07:07 -0500

    actualizacion

--- application/modules/templates/views/index.php
+++ application/modules/templates/views/index.php
@@ -145,6 +145,7 @@ $this->load->view('header', $data);
             </div>
         </div>
     <?php } ?>
+            
     <div class="row">
         <div class="col-md-8  col-sm-8 zonacontenido">
             <div class="row margen0  content ">
