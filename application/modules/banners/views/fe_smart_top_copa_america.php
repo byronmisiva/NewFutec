d24832a9a04b8f88abff3098a7a73c759e33ec21
mivida1985 <jortiz@misiva.com.ec>
2016-06-06 20:04:54 -0500

    actualizacion de codigo

--- application/modules/banners/views/fe_smart_top_copa_america.php
+++ application/modules/banners/views/fe_smart_top_copa_america.php
@@ -1,6 +1,12 @@
 <!-- /1022247/FE_BRAND_SMART_TOPcopa america-->
-<div id='div-gpt-ad-1432051647687-6' style='height:50px; width:100%;' class="respiframe" >
+<!-- <div id='div-gpt-ad-1432051647687-6' style='height:50px; width:100%;' class="respiframe" >
     <script type='text/javascript'>
         googletag.cmd.push(function() { googletag.display('div-gpt-ad-1432051647687-6'); });
     </script>
+</div>-->
+
+
+<div style="position:relative;width:100%;height: 50px;">
+<iframe src="http://www.misiva.com.ec/banners/html5/mobile_copa_america/" width="100%" height="50" frameborder="0" ></iframe>
+<div style="position: absolute;top:0;left: 0;width: 100%; height: 50px;cursor: pointer;" onclick="location.href='<?php echo base_url()?>copa-america'"></div>
 </div>
