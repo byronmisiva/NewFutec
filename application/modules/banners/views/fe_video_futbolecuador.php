c8a2628982df5793b65966002c39460fc0530528
mivida1985 <jortiz@misiva.com.ec>
2016-06-10 16:57:57 -0500

    catualizado

--- application/modules/banners/views/fe_video_futbolecuador.php
+++ application/modules/banners/views/fe_video_futbolecuador.php
@@ -27,18 +27,18 @@ if (in_array($idtipo, $tipo)) { ?>
 				 1000
 				 );
 	}, 240000);	
+	
 	var sw =0;
 	function activarVideo(){
 		if (sw == 0){
 			if(screen.width > 600){
-				 $("#div-gpt-ad-1457102356654-0").animate(
-						 { height: 'toggle'},
-						 1000
-						 );
-					return 1;
-				}else{
+			 $("#div-gpt-ad-1457102356654-0").animate(
+				 { height: 'toggle'},1000
+			 );
+			return 1;
+			}else{
 					return 0;
-					}
+			}
 				sw++;
 		}		
 	};
