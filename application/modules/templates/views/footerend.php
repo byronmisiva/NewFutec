7d4277ea2c9a6ff2cdada58da4e93ff2ffcaff32
mivida1985 <jortiz@misiva.com.ec>
2016-06-14 18:00:58 -0500

    actualizar

--- application/modules/templates/views/footerend.php
+++ application/modules/templates/views/footerend.php
@@ -128,7 +128,7 @@ function activarPbl(videoName){
 		  scale: 'auto', // Scale based on viewport size (set to 1 to disable)
 		  speedIn: 300, // Show animation speed of the banner
 		  speedOut: 400, // Close animation speed of the banner
-		  daysHidden: 15, // Duration to hide the banner after being closed (0 = always show banner)
+		  daysHidden: 0, // Duration to hide the banner after being closed (0 = always show banner)
 		  daysReminder: 90, // Duration to hide the banner after "VIEW" is clicked *separate from when the close button is clicked* (0 = always show banner)
 		  force: null, // Choose 'ios', 'android' or 'windows'. Don't do a browser check, just always show this banner
 		  hideOnInstall: true, // Hide the banner after "VIEW" is clicked.
@@ -136,10 +136,10 @@ function activarPbl(videoName){
 		  iOSUniversalApp: true, // If the iOS App is a universal app for both iPad and iPhone, display Smart Banner to iPad users, too.      
 		  appendToSelector: 'body', //Append the banner to a specific selector
 		  onInstall: function() {
-		     alert('Click install');
+		     //alert('Click install');
 		  },
 		  onClose: function() {
-		     alert('Click close');
+		     //alert('Click close');
 		  }
 		});
 	</script>
