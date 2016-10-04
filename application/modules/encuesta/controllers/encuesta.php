0ade7b2cb5e700ab078560d7895a1452f2beeaa5
mivida1985 <jortiz@misiva.com.ec>
2016-10-04 12:25:12 -0500

    actualizacion modulo encuesta

--- application/modules/encuesta/controllers/encuesta.php
+++ application/modules/encuesta/controllers/encuesta.php
@@ -13,4 +13,34 @@ class Encuesta extends MY_Controller
     	echo json_encode($this->mdl_encuesta->getEncuesta());        
     }
     
+    public function getFormulario()
+    {
+    	return $this->load->view("index",$data, TRUE);
+    }
+    
+    public function listaNoticia($mes,$year)
+    {    	
+    	$this->load->module('contenido');
+    	$this->load->module('noticias');
+    	$data['lista'] = $this->noticias->viewLista($mes,$year);
+    	$this->load->view("lista",$data);
+    }
+    
+	public function pushVotos()
+    {
+    	$data = json_decode( $_POST["datos"] );
+    	for ($x=0; $x<count($data);$x++){
+    		$this->mdl_encuesta->sumVoto($data[$x]);
+    	}
+    	
+    	$dato["registro"]=$this->mdl_encuesta->sumRegistro();
+    	$dato["arquero"]=$this->mdl_encuesta->getLista("ARQ");
+    	$dato["defensa"]=$this->mdl_encuesta->getLista("DEF");
+    	$dato["volantes"]=$this->mdl_encuesta->getLista("VOL");
+    	$dato["delantero"]=$this->mdl_encuesta->getLista("DEL");
+    	echo "<pre>";
+    	var_dump($dato);
+    	echo json_encode($data);
+    }
+    
 }
--- application/modules/encuesta/models/mdl_encuesta.php
+++ application/modules/encuesta/models/mdl_encuesta.php
@@ -29,5 +29,41 @@ class Mdl_encuesta extends MY_Model{
 			return $query->result();
 		}				 			
 	   }
+	   
+	   function sumVoto($id){
+	   	$query = $this->db->query('SELECT * FROM lista_formacion where id = '.$id);
+	   	$resultado = current($query->result());	 
+	   	$votos = (int)$resultado->voto;
+	   	$votos = $votos + 1;	   	
+	   	$this->db->query("UPDATE lista_formacion SET voto = ".$votos." WHERE id = ".$id);	   	  	
+	   	
+	   }
+	   
+	   function getNomina(){
+	   $query = $this->db->query('SELECT frm.id,eq.first_name,eq.last_name,frm.imagen , frm.posicion,voto, frm.checked
+								   FROM lista_formacion AS frm, players as eq 
+                                   where eq.id=frm.player_id
+                                   order by frm.voto;');		
+		return $query->result();			
+	   }
+	   
+	   function getLista($pos){
+	   	if ($pos == "ARQ"){
+	   		$limit = 1;
+	   	}else if($pos == "DEF"){
+	   		$limit = 4;
+	   	}else if($pos == "VOL"){
+	   		$limit = 4;
+	   	}else if($pos == "DEL"){
+	   		$limit = 2;
+	   	}
+	   	$query = $this->db->query('SELECT frm.id,eq.first_name,eq.last_name,frm.imagen , frm.posicion,voto, frm.checked
+								   FROM lista_formacion AS frm, players as eq
+                                   where eq.id=frm.player_id
+	   							   and frm.posicion = "'.$pos.'"
+                                   order by frm.voto;
+	   							   limit '.$limit);
+	   	return $query->result();
+	   }
 	
 }
