a3be074f0833d9dad141301b0e9812ada0746f35
mivida1985 <jortiz@misiva.com.ec>
2016-10-13 18:31:24 -0500

    actualizar

--- application/modules/templates/views/footerend.php
+++ application/modules/templates/views/footerend.php
@@ -71,7 +71,7 @@ function activarPbl(){
 <?php  if(($this->uri->segment(1) == "site") && ($this->uri->segment(2) == "noticia")){ ?>
 	$(".body-principal").attr('onmouseout','verificarLocalStorage()');
 	function verificarLocalStorage(){
-		if ( parseInt( window.localStorage['estadoVisita'] ) == 1){	
+		if ( parseInt( window.localStorage['estadoVisita'] ) == 0){	
 			$(".bannerMini").show();
 		}else if ( (parseInt( window.localStorage['estadoVisita'] ) % 2 ) == 1) {
 			$(".bannerMayor").show();
