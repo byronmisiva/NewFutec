263171f0f93b8b390d49fc6a3cdebcaffdb2989b
mivida1985 <jortiz@misiva.com.ec>
2016-10-13 19:36:45 -0500

    actualizar

--- application/modules/encuesta/views/avisonotciacentral.php
+++ application/modules/encuesta/views/avisonotciacentral.php
@@ -1,8 +1,15 @@
+<?php var_dump($principal);?>
+
+<?php var_dump($principal[0]);?>
+
+<?php var_dump($principal[0]->title);?>
+<?php var_dump($principal["title"]);?>
+
 <div class="row">
 	<div class="col-lg-6">
 		<div class="row">
 			<div class="col-lg-12">
-				<img src="<?php echo base_url().$principal[0]->thumb300?>" />
+				<img src="<?php echo "http://www.futbolecuador.com/".$principal[0]->thumb300?>" />
 			</div>
 			<div class="col-lg-12">
 				<?php $principal[0]->title?>"
