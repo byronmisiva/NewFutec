10c902b2ba921aef367cfa44d7266135e6275965
mivida1985 <jortiz@misiva.com.ec>
2016-10-13 19:32:28 -0500

    actualizar

--- application/modules/encuesta/views/avisonotciacentral.php
+++ application/modules/encuesta/views/avisonotciacentral.php
@@ -1,6 +1,16 @@
 <div class="row">
 	<div class="col-lg-6">
-		<?php echo var_dump($principal);?>
+		<div class="row">
+			<div class="col-lg-12">
+				<img src="<?php echo base_url().$principal[0]->thumb300?>" />
+			</div>
+			<div class="col-lg-12">
+				<?php $principal[0]->title?>"
+			</div>
+			<div class="col-lg-12">
+				<?php $principal[0]->lead?>"
+			</div>
+		</div>
 	</div>
 	<div class="col-lg-6">
 		<?php echo $loMasLeido;?>
