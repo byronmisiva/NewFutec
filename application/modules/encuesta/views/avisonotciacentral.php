364da07da9121584ee1ef0e30a720ea4ce004f92
mivida1985 <jortiz@misiva.com.ec>
2016-10-13 20:47:09 -0500

    actualizar

--- application/modules/encuesta/views/avisonotciacentral.php
+++ application/modules/encuesta/views/avisonotciacentral.php
@@ -4,15 +4,15 @@ $link = base_url.'site/noticia/'.$this->contenido->_urlFriendly ($principal[0]->
 <div class="row">
 	<div class="col-lg-6">
 		<div class="row">
-		<a href="<?php echo $link?>" >
+		<a href="<?php echo $link?>">
 			<div class="col-lg-12 text-center" style="background-color:#fff; padding-top: 10px">
 				<img src="<?php echo "http://www.futbolecuador.com/".$principal[0]->thumb300?>" width="100%" />
 			</div>
 			<div class="col-lg-12" style="background-color:#fff; ">
-				<h2 style="font-size: 18px;"><?php echo $principal[0]->title?>"</h2>
+				<h2 style="font-size: 18px;color:#000;"><?php echo $principal[0]->title?>"</h2>
 			</div>
-			<div class="col-lg-12" style="background-color:#fff; ">
-				<p><?php echo $principal[0]->subtitle?>"</p>
+			<div class="col-lg-12" style="background-color:#fff;color:#000; ">
+				<p style="color:#000;"><?php echo $principal[0]->subtitle?>"</p>
 			</div>
 			</a>
 		</div>
