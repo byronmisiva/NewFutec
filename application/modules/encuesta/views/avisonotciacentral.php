648905a301b50e666bfd3ca1a9c568dc658d28d8
mivida1985 <jortiz@misiva.com.ec>
2016-10-13 20:45:09 -0500

    actualizar

--- application/modules/encuesta/views/avisonotciacentral.php
+++ application/modules/encuesta/views/avisonotciacentral.php
@@ -1,7 +1,10 @@
-
+<?php 
+$link = base_url.'site/noticia/'.$this->contenido->_urlFriendly ($principal[0]->title).'/'.$principal[0]->id;
+?>
 <div class="row">
 	<div class="col-lg-6">
 		<div class="row">
+		<a href="<?php echo $link?>" >
 			<div class="col-lg-12 text-center" style="background-color:#fff; padding-top: 10px">
 				<img src="<?php echo "http://www.futbolecuador.com/".$principal[0]->thumb300?>" width="100%" />
 			</div>
@@ -9,8 +12,9 @@
 				<h2 style="font-size: 18px;"><?php echo $principal[0]->title?>"</h2>
 			</div>
 			<div class="col-lg-12" style="background-color:#fff; ">
-				<p><?php echo $principal[0]->lead?>"</p>
+				<p><?php echo $principal[0]->subtitle?>"</p>
 			</div>
+			</a>
 		</div>
 	</div>
 	<div class="col-lg-6">
