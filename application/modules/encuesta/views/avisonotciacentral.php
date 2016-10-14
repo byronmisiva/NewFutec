653ecb7e24726a8395c9c4fb6e713466a2764002
mivida1985 <jortiz@misiva.com.ec>
2016-10-13 20:39:12 -0500

    actualizar

--- application/modules/encuesta/views/avisonotciacentral.php
+++ application/modules/encuesta/views/avisonotciacentral.php
@@ -2,11 +2,11 @@
 <div class="row">
 	<div class="col-lg-6">
 		<div class="row">
-			<div class="col-lg-12 text-center" style="background-color:#fff; ">
+			<div class="col-lg-12 text-center" style="background-color:#fff; padding-top: 10px">
 				<img src="<?php echo "http://www.futbolecuador.com/".$principal[0]->thumb300?>" width="100%" />
 			</div>
 			<div class="col-lg-12" style="background-color:#fff; ">
-				<h2><?php echo $principal[0]->title?>"</h2>
+				<h2 style="font-size: 18px;"><?php echo $principal[0]->title?>"</h2>
 			</div>
 			<div class="col-lg-12" style="background-color:#fff; ">
 				<p><?php echo $principal[0]->lead?>"</p>
