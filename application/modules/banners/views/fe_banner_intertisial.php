7191f4728665772c08728c39f267f6d4923e099b
mivida1985 <jortiz@misiva.com.ec>
2016-10-14 15:52:04 -0500

    actualizar

--- application/modules/banners/views/fe_banner_intertisial.php
+++ application/modules/banners/views/fe_banner_intertisial.php
@@ -6,27 +6,47 @@
 <!-- Placement ID: c3fbb26d-91e5-4e54-b64c-0d3eea13aa31 -->
 <!-- Placement Size: 375x667 -->
 <!-- Ad Server: DFP -->
-<div style="display:none !important;position: fixed;z-index: 10000000;top: 0;left: 0;"
+<style>
+.banner_interstisial{
+	position: fixed;z-index: 10000000;top: 0;left: 0;
+}
+
+</style>
+<div class="banner_interstisial"  >
+	<div class="row">
+		<div class="col-lg-12">
+			<div onclick="$('.banner_interstisial').hide()" style=" width: 100px;height:55px;">Cerrar</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-12">
+			<div style="display:none !important;"
 id="flite-click-macro-c3fbb26d-91e5-4e54-b64c-0d3eea13aa31">%%CLICK_URL_UNESC%%</div>
 
-<script>
-    (function() {
-        var guid = "c3fbb26d-91e5-4e54-b64c-0d3eea13aa31", o = window, r = "", m, s = "http:", e = encodeURIComponent, x = 0, f = document.createElement("script");
-        o.FLITE=o.FLITE || {};
-        o.FLITE.config = o.FLITE.config || {};
-        o.FLITE.config[guid] = o.FLITE.config[guid] || {};
-        o.FLITE.config[guid].cb = Math.random();
-        o.FLITE.config[guid].ts = (+ new Date());
-        try{ r = (top===self && top.location) ? top.location.href : document.referrer || (top.location && top.location.href) || "";}catch(er){x=1}
-        try{ s = o.location && o.location.protocol === "https:"? o.location.protocol : s;}catch(er){x+=2}
-        try{ m = r.match(new RegExp("[A-Za-z]+:[/][/][A-Za-z0-9.-]+")); } catch(er) {x+=4}
-        try{ var fm = document.getElementById("flite-click-macro-" + guid); var c = (fm && (fm.innerText || fm.textContent || fm.innerHTML)) || ""; c = c.replace(/^\s+|\s+$/g, ""); o.FLITE.config[guid].ct = c; } catch(err) {x+=8}
-        f.src = [s,"//r.flite.com/syndication/uscript.js?i=",e(guid),"&v=3","","&x=us",x,"&cb=",o.FLITE.config[guid].cb,"&d=",e((m && m[0]) || r), "&tz=", (new Date()).getTimezoneOffset()].join("");
-        document.write(f.outerHTML);
-        })();
-</script>
-<noscript>
-    <a style="text-decoration:none;display:block;border:0;" href="//r.flite.com/syndication/backuplink/i/c3fbb26d-91e5-4e54-b64c-0d3eea13aa31?ct=%%CLICK_URL_ESC%%" target="_blank">
-        <img border="0" src="//r.flite.com/syndication/backupimage/i/c3fbb26d-91e5-4e54-b64c-0d3eea13aa31?at="/>
-    </a>
-</noscript>
+			<script>
+			    (function() {
+			        var guid = "c3fbb26d-91e5-4e54-b64c-0d3eea13aa31", o = window, r = "", m, s = "http:", e = encodeURIComponent, x = 0, f = document.createElement("script");
+			        o.FLITE=o.FLITE || {};
+			        o.FLITE.config = o.FLITE.config || {};
+			        o.FLITE.config[guid] = o.FLITE.config[guid] || {};
+			        o.FLITE.config[guid].cb = Math.random();
+			        o.FLITE.config[guid].ts = (+ new Date());
+			        try{ r = (top===self && top.location) ? top.location.href : document.referrer || (top.location && top.location.href) || "";}catch(er){x=1}
+			        try{ s = o.location && o.location.protocol === "https:"? o.location.protocol : s;}catch(er){x+=2}
+			        try{ m = r.match(new RegExp("[A-Za-z]+:[/][/][A-Za-z0-9.-]+")); } catch(er) {x+=4}
+			        try{ var fm = document.getElementById("flite-click-macro-" + guid); var c = (fm && (fm.innerText || fm.textContent || fm.innerHTML)) || ""; c = c.replace(/^\s+|\s+$/g, ""); o.FLITE.config[guid].ct = c; } catch(err) {x+=8}
+			        f.src = [s,"//r.flite.com/syndication/uscript.js?i=",e(guid),"&v=3","","&x=us",x,"&cb=",o.FLITE.config[guid].cb,"&d=",e((m && m[0]) || r), "&tz=", (new Date()).getTimezoneOffset()].join("");
+			        document.write(f.outerHTML);
+			        })();
+			</script>
+			<noscript>
+			    <a style="text-decoration:none;display:block;border:0;" href="//r.flite.com/syndication/backuplink/i/c3fbb26d-91e5-4e54-b64c-0d3eea13aa31?ct=%%CLICK_URL_ESC%%" target="_blank">
+			        <img border="0" src="//r.flite.com/syndication/backupimage/i/c3fbb26d-91e5-4e54-b64c-0d3eea13aa31?at="/>
+			    </a>
+			</noscript>
+		</div>
+	</div>	
+	
+
+</div>
+
