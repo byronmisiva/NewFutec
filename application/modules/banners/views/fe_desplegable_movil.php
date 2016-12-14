4726f15e2abb6d4469b6cd2a6d0be14041d6832b
mivida1985 <jortiz@misiva.com.ec>
2016-12-13 19:51:47 -0500

    Actualizacion banner flotante

--- application/modules/banners/views/fe_desplegable_movil.php
+++ application/modules/banners/views/fe_desplegable_movil.php
@@ -1,11 +1,12 @@
 <div class="publicidadFlotante">
-    <!-- <div id="Stage" class="EDGE-3751729"></div>--->
+    <div id="Stage" class="EDGE-3751729">
+    </div>
 </div>
 <!--   script para banner solo caso home movil-->
 <?php
 $idtipo = $this->uri->segment(2);
 $tipo = array("movil");
 if (in_array($idtipo, $tipo)) {?>
-   <!-- <script type="text/javascript" src="<?= base_url() ?>assets/js/banner/banner_edgePreload.js"></script>-->
+    <script type="text/javascript" src="<?= base_url() ?>assets/js/banner/banner_edgePreload.js"></script>
 <?php
 }?>
