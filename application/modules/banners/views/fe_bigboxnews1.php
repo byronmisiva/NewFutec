a9b7dbce6331c8098bf5effea05462e879219f50
mivida1985 <jortiz@misiva.com.ec>
2016-12-19 18:57:26 -0500

    Verificacion de urisegmet

--- application/modules/banners/views/fe_bigboxnews1.php
+++ application/modules/banners/views/fe_bigboxnews1.php
@@ -1,10 +1,13 @@
+<?php 
+$idtipo = $this->uri->segment(2);
+echo "segmento: ".$idtipo;
+?>
 <!-- FE_NEW_RECTANGLE_1 -->
 <div id='div-gpt-ad-1413414586192-4' class="banner300x250 hidden-xs" >
     <script type='text/javascript'>
         googletag.cmd.push(function() { googletag.display('div-gpt-ad-1413414586192-4'); });
     </script>
 </div>
-
 <!-- FE_SMART_MIDDLE 
 <div id='div-gpt-ad-1383593619381-3'   class="visible-xs-block respiframe" >
     <script type='text/javascript'>
