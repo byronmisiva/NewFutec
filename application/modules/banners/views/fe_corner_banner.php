b5851b4234bdf39694b8d8f9341e424466a5ba85
mivida1985 <jortiz@misiva.com.ec>
2017-01-18 16:59:22 -0500

    actualizacion de codigo para desplegar banner

--- application/modules/banners/views/fe_corner_banner.php
+++ application/modules/banners/views/fe_corner_banner.php
@@ -9,20 +9,17 @@
 	<script src="http://vast-cdn.mtngt.com/video-formats/prod/corner/scripts/code-corner-dfp.min.js"></script>
 </div>
 
-<div style="    position: absolute;
-    width: 600px;
-    height: 400px;
-    border: 1px solid red;
-    margin: 10% auto;
-    z-index: 100000000;
-    top: 150px;
-    left: 327px;">
+<div style="position: absolute;width: 1px;height: 1px;border: 1px solid red;margin: 0 auto;z-index: 100000000;
+    ">
 <!-- /1022247/InRead_600x400 -->
 <div id='div-gpt-ad-1484773297502-0'>
 <script>
 	googletag.cmd.push(function() { googletag.display('div-gpt-ad-1484773297502-0'); });
 </script>
 </div>
-
+<!--banner inreader  -->
+<script>var adParams = {playerId:1302501220, demandWidth: 600, demandHeight: 400 }</script>
+<script src="http://vast-cdn.mtngt.com/video-formats/prod/in-content/scripts/in-content-dfp.min.js"></script>
+<!-- fin inreader -->
 </div>
 
