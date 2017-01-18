18d0a2377b9399d83826eb8f614e315a54959a8c
mivida1985 <jortiz@misiva.com.ec>
2017-01-18 16:02:44 -0500

    actualizacion de codigo para desplegar banner

--- application/modules/banners/views/fe_corner_banner.php
+++ application/modules/banners/views/fe_corner_banner.php
@@ -1,1 +1,9 @@
-<div style="position: absolute;width: 400px; height: 300px;top:0;right:0;border:1px solid red;"></div>
+<div style="position: absolute;width: 400px; height: 300px;top:0;right:0;border:1px solid red;">
+<!-- /1022247/InRead_600x400 -->
+<div id='div-gpt-ad-1484773297502-0'>
+<script>
+	googletag.cmd.push(function() { googletag.display('div-gpt-ad-1484773297502-0'); });
+</script>
+</div>
+</div>
+
