8f0a6fbaa11b3ac335c26e042cdb1db293d61c9a
mivida1985 <jortiz@misiva.com.ec>
2017-01-18 16:52:10 -0500

    actualizacion de codigo para desplegar banner

--- application/modules/banners/views/fe_corner_banner.php
+++ application/modules/banners/views/fe_corner_banner.php
@@ -1,3 +1,14 @@
+<div style="position: absolute;width: 400px;height: 300px;border: 1px solid red;right:0;top:0;z-index: 100000000;">
+<!-- /1022247/CornerVideo -->
+<div id='div-gpt-ad-1484776299682-0'>
+	<script>
+		googletag.cmd.push(function() { googletag.display('div-gpt-ad-1484776299682-0'); });
+	</script>
+</div>
+	<script>var adParams = {playerId:1302501220, width: 400, height: 300 }</script>
+	<script src="http://vast-cdn.mtngt.com/video-formats/prod/corner/scripts/code-corner-dfp.min.js"></script>
+</div>
+
 <div style="    position: absolute;
     width: 600px;
     height: 400px;
