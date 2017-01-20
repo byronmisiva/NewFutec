1f605134de7d77b074ce6bfb1b3f5c943bc97184
mivida1985 <jortiz@misiva.com.ec>
2017-01-19 20:40:26 -0500

    actualziacion botones compartir

--- application/modules/contenido/views/sidebar.php
+++ application/modules/contenido/views/sidebar.php
@@ -24,7 +24,7 @@
     </span>
     </div>
     <div class="col-md-3 col-xs-3  ">
-    <span class="social-pos">
+    <!-- <span class="social-pos">
         <span class="ig-follow" data-id="a83ed5" data-handle="futbolecuadorcom" data-count="false" data-size="small"
               data-username="false"></span>
                     <script>(function (d, t) {
@@ -32,7 +32,13 @@
                             g.src = "//x.instagramfollowbutton.com/follow.js";
                             s.parentNode.insertBefore(g, s);
                         }(document, "script"));</script>
-    </span>
+    </span>-->
+    
+    <style><!--
+		.ig-b- { display: inline-block; } .ig-b- img { visibility: hidden; } .ig-b-:hover { background-position: 0 -60px; } .ig-b-:active { background-position: 0 -120px; } .ig-b-48 { width: 48px; height: 48px; background: url(//badges.instagram.com/static/images/ig-badge-sprite-48.png) no-repeat 0 0; } @media only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (min--moz-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2 / 1), only screen and (min-device-pixel-ratio: 2), only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx) { .ig-b-48 { background-image: url(//badges.instagram.com/static/images/ig-badge-sprite-48@2x.png); background-size: 60px 178px; } }
+--></style>
+    <a class="ig-b- ig-b-48" href="https://www.instagram.com/futbolecuadorcom/?ref=badge"><img alt="Instagram" src="//badges.instagram.com/static/images/ig-badge-48.png" /></a>
+    
     </div>
     <div class="col-md-3 col-xs-3  ">    
 		<a href="https://soundcloud.com/user-13413667" target="_blank">
