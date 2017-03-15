c289c798f7f1160d644cb4005368e0d957f0b739
mivida1985 <jortiz@misiva.com.ec>
2017-03-15 17:01:50 -0500

    actualizacion codigo

--- application/modules/banners/views/fe_banner_intertisial.php
+++ application/modules/banners/views/fe_banner_intertisial.php
@@ -1,6 +1,6 @@
 <!-- /1022247/Interstitial_Fullscreen_FE-Mobile -->
-<div id='div-gpt-ad-1489613603020-0'>
+<div id='div-gpt-ad-1489615543513-0' style='height:1px; width:1px;'>
 <script>
-googletag.cmd.push(function() { googletag.display('div-gpt-ad-1489613603020-0'); });
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1489615543513-0'); });
 </script>
 </div>
