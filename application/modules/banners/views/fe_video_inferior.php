e94cb8dd7ee3b7250c66ff33493d2068ff50a2a8
mivida1985 <jortiz@misiva.com.ec>
2017-07-11 13:27:52 -0500

    banner 1x1

---
+++ application/modules/banners/views/fe_video_inferior.php
@@ -0,0 +1,6 @@
+<!-- /1022247/LatinOn_1x1_USA -->
+<div id='div-gpt-ad-1499696905612-0' style='height:1px; width:1px;'>
+	<script>
+		googletag.cmd.push(function() { googletag.display('div-gpt-ad-1499696905612-0'); });
+	</script>
+</div>
