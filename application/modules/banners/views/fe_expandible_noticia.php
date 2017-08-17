4c103325c794290a3074dca46fd28ed3c7d360a2
mivida1985 <jortiz@misiva.com.ec>
2017-08-17 13:45:53 -0500

    actualizacion de codigo del banner al mismo se agrego un codigo para que se redimensione a 370 de alto

--- application/modules/banners/views/fe_expandible_noticia.php
+++ application/modules/banners/views/fe_expandible_noticia.php
@@ -2,8 +2,10 @@
 <div id='div-gpt-ad-1502927248158-0' style='height:380px; width:320px;'>
 <script>
 	googletag.cmd.push(function() { googletag.display('div-gpt-ad-1502927248158-0'); });
-	setTimeout(function(){ 
-		$("#google_ads_iframe_/1022247/20-Noticia_Expandible_0").css("height","370px;");
-	}, 3000);
+	setTimeout(function(){
+		console.debug($("#google_ads_iframe_/1022247/20-Noticia_Expandible_0__container__"));
+		console.debug($("#google_ads_iframe_/1022247/20-Noticia_Expandible_0"));
+		$("#google_ads_iframe_/1022247/20-Noticia_Expandible_0").attr("height","370");
+	}, 2000);
 </script>
 </div>
