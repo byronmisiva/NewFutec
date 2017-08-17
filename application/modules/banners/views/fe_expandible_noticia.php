3b3997b1eda17fa03be97080f58c180559063bb9
mivida1985 <jortiz@misiva.com.ec>
2017-08-16 21:30:13 -0500

    insercion de codigo para que se despliegue banner noticia expandible

---
+++ application/modules/banners/views/fe_expandible_noticia.php
@@ -0,0 +1,6 @@
+<!-- /1022247/20-Noticia_Expandible -->
+<div id='div-gpt-ad-1502927248158-0' style='height:340px; width:320px;'>
+<script>
+	googletag.cmd.push(function() { googletag.display('div-gpt-ad-1502927248158-0'); });
+</script>
+</div>
