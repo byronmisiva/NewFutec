f098c5c71d2d6fd004e7c33cb80f865b538857ce
mivida1985 <jortiz@misiva.com.ec>
2017-08-21 11:24:34 -0500

    actualizacion codigo

--- application/modules/banners/views/fe_expandible_noticia.php
+++ application/modules/banners/views/fe_expandible_noticia.php
@@ -1,4 +1,4 @@
-<!-- /1022247/20-Noticia_Expandible -->
+<!-- /1022247/20-Noticia_Expandible 
 <div id='div-gpt-ad-1502927248158-0' style='height:380px; width:320px;'>
 <script>
 	googletag.cmd.push(function() { googletag.display('div-gpt-ad-1502927248158-0'); });
@@ -9,4 +9,17 @@
 		$(iframe).attr("height","370");
 	}, 2500);
 </script>
+</div>-->
+
+<div id='20-Noticia_Expandible' style='margin-top: 5px; width: 300px; height: 370px;'>
+    <script type='text/javascript'>GA_googleFillSlot("20-Noticia_Expandible");</script>
 </div>
+<script>
+	
+	/*setTimeout(function(){
+		var elem = $("#FE_NOTICIA_EXPANDIBLE");
+		var iframe = elem[0].children[1];
+		var iframe = $(iframe)[0].childNodes[0];
+		$(iframe).attr("height","370");
+	}, 2500);*/
+</script>
