5782ee0092dcd5838f9123aa62efd48552fc95ca
mivida1985 <jortiz@misiva.com.ec>
2017-08-21 15:38:54 -0500

    actualizacion de espacio

--- application/modules/banners/views/fe_expandible_noticia.php
+++ application/modules/banners/views/fe_expandible_noticia.php
@@ -1,24 +1,49 @@
 <!-- /1022247/20-Noticia_Expandible 
-<div id='div-gpt-ad-1503088296523-0' style='height:380px; width:320px;'>
+<div id='div-gpt-ad-1502927248158-0' style='height:380px; width:320px;'>
 <script>
-	googletag.cmd.push(function() { googletag.display('div-gpt-ad-1503088296523-0'); });
+	googletag.cmd.push(function() { googletag.display('div-gpt-ad-1502927248158-0'); });
 	setTimeout(function(){
-		var elem = $("#div-gpt-ad-1503088296523-0");
+		var elem = $("#div-gpt-ad-1502927248158-0");
 		var iframe = elem[0].children[1];
 		var iframe = $(iframe)[0].childNodes[0];
 		$(iframe).attr("height","370");
 	}, 2500);
 </script>
 </div>-->
-<!-- /1022247/20-Noticia_Expandible -->
-<!-- <div id='div-gpt-ad-1502927248158-0' style='height:340px; width:320px;'>
-<script>
-	googletag.cmd.push(function() { googletag.display('div-gpt-ad-1502927248158-0'); });
-</script>
-</div>-->
+<!--  orde tecnomega
+2598-->
+<style>
+/*.gwd-pagedeck>.gwd-page {
+    position: fixed;
+    top: 0;
+    left: 0; 
+    z-index: 10000;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0,0,0,0.3);
+}
 
-<div id='20-Noticia_Expandible' style='width: 300px; height: 340px;'>
+.gwd-page-container {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}*/
+
+#expanded-page{
+        width: 100%;
+    height: 100%;
+    background-color: rgba(0,0,0,0.5);
+    position: fixed !important;
+    z-index: 100000;
+    top: 0 !important;
+    left: 0 !important;
+}
+
+.gwd-page-content.expanded.gwd-div-g7oi{
+	margin: 0 auto !important;
+	position: relative !important;
+}
+</style>
+<div id='20-Noticia_Expandible' style='margin-top: 5px; width: 320px; height: 370px;'>
     <script type='text/javascript'>GA_googleFillSlot("20-Noticia_Expandible");</script>
 </div>
-
-
