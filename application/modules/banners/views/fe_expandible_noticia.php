9539e18a318cf01facbfa4d44c768e90930d00bf
mivida1985 <jortiz@misiva.com.ec>
2017-08-21 11:43:56 -0500

    actualizacion codigo

--- application/modules/banners/views/fe_expandible_noticia.php
+++ application/modules/banners/views/fe_expandible_noticia.php
@@ -11,6 +11,38 @@
 </script>
 </div>-->
 
+<style>
+.gwd-pagedeck>.gwd-page {
+    position: fixed;
+    top: 0;
+    left: 0; 
+    z-index: 10000;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0,0,0,0.3);
+}
+
+.gwd-page-container {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+
+#expanded-page{
+        width: 100%;
+    height: 100%;
+    background-color: rgba(0,0,0,0.5);
+    position: fixed !important;
+    z-index: 100000;
+    top: 0 !important;
+    left: 0 !important;
+}
+
+.gwd-page-content.expanded.gwd-div-g7oi{
+	margin: 0 auto !important;
+	position: relative !important;
+}
+</style>
 <div id='20-Noticia_Expandible' style='margin-top: 5px; width: 300px; height: 370px;'>
     <script type='text/javascript'>GA_googleFillSlot("20-Noticia_Expandible");</script>
 </div>
@@ -23,3 +55,4 @@
 		$(iframe).attr("height","370");
 	}, 2500);*/
 </script>
+
