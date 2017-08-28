31a07124ae53f6b43d0e338c92a0d383c25814bf
mivida1985 <jortiz@misiva.com.ec>
2017-08-28 10:25:13 -0500

    declaracion espacio

--- application/modules/templates/views/header.php
+++ application/modules/templates/views/header.php
@@ -395,8 +395,8 @@ googletag.defineSlot('/1022247/FE_NEW_RECTANGLE_3', [300, 250], 'div-gpt-ad-1466
 
 <script type='text/javascript' src='http://partner.googleadservices.com/gampad/google_service.js'></script>
 <script type='text/javascript'>
-    //GS_googleAddAdSenseService("ca-pub-2857298972794488");
-    //GS_googleEnableAllServices();
+    GS_googleAddAdSenseService("ca-pub-2857298972794488");
+    GS_googleEnableAllServices();
 </script>
 <script type='text/javascript'>
     GA_googleAddSlot("ca-pub-2857298972794488", "FE_HP_1");   
