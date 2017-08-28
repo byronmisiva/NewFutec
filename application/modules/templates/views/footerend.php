64bc1c5a2412bdea962d87503a6850a90ba22873
mivida1985 <jortiz@misiva.com.ec>
2017-08-28 10:14:57 -0500

    llamada js

--- application/modules/templates/views/footerend.php
+++ application/modules/templates/views/footerend.php
@@ -1,4 +1,3 @@
-
 <link href="<?php echo base_url('assets/css/fhmm.css') ?>" rel="stylesheet">
 <link href="<?php echo base_url('assets/css/font-awesome.css') ?>" rel="stylesheet">
 <?php if($verMobile == "0"){ ?>
@@ -24,11 +23,11 @@
 
   </style>
   <script>
-  /*	AdobeEdge.loadComposition(baseUrl+'unionlayer/kia_marzo/index', 'EDGE-518637820', {
+  	AdobeEdge.loadComposition(baseUrl+'unionlayer/kia_marzo/index', 'EDGE-518637820', {
 	    scaleToFit: "none",centerStage: "none",minW: "0px",maxW: "undefined",
 	    width: "1330px",
 	    height: "600px"},
-	     {dom: [ ]}, {dom: [ ]});*/
+	     {dom: [ ]}, {dom: [ ]});
   </script>
     <div class="pbl-union" >
 	   <div id="pbl-coke">
@@ -36,9 +35,9 @@
 	    </div>
 	</div>
 	<script>
-    /*AdobeEdge.loadComposition(baseUrl+'unionlayer/reto/index', 'EDGE-24483998', {
+    AdobeEdge.loadComposition(baseUrl+'unionlayer/reto/index', 'EDGE-24483998', {
     scaleToFit: "none",centerStage: "none",minW: "0px",maxW: "undefined",
-    width: "1330px",height: "600px"}, {dom: [ ]}, {dom: [ ]});*/
+    width: "1330px",height: "600px"}, {dom: [ ]}, {dom: [ ]});
   </script>
   <div class="pbl-union2" >
 	    <div id="pbl-coke">
@@ -46,8 +45,8 @@
 	    </div>
 	</div>
 	<script>
-	    /*AdobeEdge.loadComposition(baseUrl+'unionlayer/patio_tuerca/uno/centro_new', 'EDGE-11811645', {
-	    scaleToFit: "none",centerStage: "none",minW: "0px",maxW: "undefined",width: "1330px",height: "600px"}, {dom: [ ]}, {dom: [ ]});*/	
+	    AdobeEdge.loadComposition(baseUrl+'unionlayer/patio_tuerca/uno/centro_new', 'EDGE-11811645', {
+	    scaleToFit: "none",centerStage: "none",minW: "0px",maxW: "undefined",width: "1330px",height: "600px"}, {dom: [ ]}, {dom: [ ]});	
     </script>
 	  <div class="pbl-union4" >
 	    <div id="pbl-coke">
@@ -55,9 +54,8 @@
 	    </div>
 	</div>
 	<script>
-	    /*AdobeEdge.loadComposition(baseUrl+'unionlayer/patio_tuerca/promo/centro_new', 'EDGE-11811615', {
-	    scaleToFit: "none",centerStage: "none",minW: "0px",maxW: "undefined",width: "1330px",height: "600px"}, {dom: [ ]}, {dom: [ ]});
-	    */	
+	    AdobeEdge.loadComposition(baseUrl+'unionlayer/patio_tuerca/promo/centro_new', 'EDGE-11811615', {
+	    scaleToFit: "none",centerStage: "none",minW: "0px",maxW: "undefined",width: "1330px",height: "600px"}, {dom: [ ]}, {dom: [ ]});	
     </script>
 	  <div class="pbl-union5" >
 	    <div id="pbl-coke">
@@ -65,9 +63,8 @@
 	    </div>
 	</div>
 	<script>
-	  /*  AdobeEdge.loadComposition(baseUrl+'unionlayer/kia/kia', 'EDGE-61983072', {
-	    scaleToFit: "none",centerStage: "none",minW: "0px",maxW: "undefined",width: "1330px",height: "600px"}, {dom: [ ]}, {dom: [ ]});
-	    */	
+	    AdobeEdge.loadComposition(baseUrl+'unionlayer/kia/kia', 'EDGE-61983072', {
+	    scaleToFit: "none",centerStage: "none",minW: "0px",maxW: "undefined",width: "1330px",height: "600px"}, {dom: [ ]}, {dom: [ ]});	
     </script>
 	<div class="pbl-union6" >
 	    <div id="pbl-coke">
@@ -188,6 +185,19 @@
 		$("#div-gpt-ad-1450734059657-1").hide();
 		$(".pbl_unionajax").fadeIn();
 		$(".pbl-union3").fadeIn();
+	}
+
+	function unionlayerSony(){
+		$(".pbl_unionajax").load("<?php echo base_url('')?>banners/fe_sony");
+		$("#div-gpt-ad-1450734059657-0").hide();
+		$("#div-gpt-ad-1450734059657-1").hide();
+		$(".pbl_unionajax").fadeIn();
+		$(".pbl-union3").fadeIn();
+	}
+
+	function unionlayerCoke(){
+		$(".pbl_unionajax").load("<?php echo base_url('')?>banners/fe_expan_coke");
+		$(".pbl_unionajax").fadeIn();
 	}
 </script>	
 <?php }?>
@@ -318,6 +328,105 @@ if (isset($fe_scritp_footer)){
 	setTimeout(function(){    	
 		verificarInstlacion();
 	    }, 3000);
+
+
+
+    // Pushwoosh.com Safari push notifications.
+    // Ensure that the user can receive Safari Push Notifications.
+    var APP_CODE = '0E44F-5F59B';          // Your Pushwoosh application code from the Control Panel
+    var WEB_SITE_PUSH_ID = 'web.futbolecuador.pushwoosh.2015';        // Your unique reverse-domain Website Push ID from the Developer Center, starts with "web."
+    var pushwooshUrl = 'https://cp.pushwoosh.com/json/1.3/';
+    var checkRemotePermission = function (permissionData) {
+        console.log(permissionData);
+        if (permissionData.permission === 'default') {
+            console.log('This is a new web service URL and its validity is unknown.');
+            window.safari.pushNotification.requestPermission(
+                pushwooshUrl + 'safari',
+                WEB_SITE_PUSH_ID,
+                { application: APP_CODE },
+                checkRemotePermission    // The callback function.
+            );
+        } else if (permissionData.permission === 'denied') {
+            console.log('The user said no.');
+        } else if (permissionData.permission === 'granted') {
+            console.log('The web service URL is a valid push provider, and the user said yes.');
+            console.log('You deviceToken is ' + permissionData.deviceToken);
+            // setTags call
+            //var tags = {"Alias": "SafariValue", "FavNumber": "98"};
+            //pushwooshSetTags(permissionData.deviceToken, tags);
+        }
+    };
+
+    window.onload = function(){
+        if ('safari' in window && 'pushNotification' in window.safari) {
+            var permissionData = window.safari.pushNotification.permission(WEB_SITE_PUSH_ID);
+            checkRemotePermission(permissionData);
+        } else {
+            console.log('Push Notifications are available for Safari browser only');
+        }
+
+        try {
+            if (navigator.userAgent.indexOf('Safari') > -1) {
+                var hashReg = /#P(.*)/,
+                    hash = decodeURIComponent(document.location.hash);
+
+                if ('safari' in window && 'pushNotification' in window.safari) {
+                    var permissionData = window.safari.pushNotification.permission(WEB_SITE_PUSH_ID);
+                }
+
+                if (hashReg.test(hash) && permissionData) {
+                    var xhr = new XMLHttpRequest(),
+                        url = pushwooshUrl + 'pushStat',
+                        params = {
+                            "request":{
+                                "application": APP_CODE,
+                                "hwid": permissionData.deviceToken,
+                                "hash": hashReg.exec(hash)[1]
+                            }
+                        };
+
+                    xhr.open('POST', url, true);
+                    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+                    xhr.send(JSON.stringify(params));
+                }
+            }
+        } catch(e) { }
+    };
+
+    function pushwooshSetTags(hwid, tags) {
+        console.log('Sending setTags call to Pushwoosh');
+        try {
+            var xhr = new XMLHttpRequest(),
+                url = pushwooshUrl + 'setTags',
+                params = {
+                    request:{
+                        application: APP_CODE,
+                        hwid: hwid,
+                        tags: tags
+                    }
+                };
+
+            xhr.open('POST', url, true);
+            xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+            xhr.send(JSON.stringify(params));
+            xhr.onload = function() {
+                if(this.status == 200) {
+                    var response = JSON.parse(this.responseText);
+                    if (response.status_code == 200) { console.log('Set tags method were successfully sent to Pushwoosh'); }
+                    else { console.log('Error occurred while sending setTags to Pushwoosh: ' + response.status_message); }
+                } else {
+                    console.log('Error occurred, status code::' + this.status);
+                }
+            };
+            xhr.onerror = function(){ console.log('Pushwoosh response status code to pushStat call in not 200'); };
+        } catch(e) {
+            console.log('Exception while sending setTags to Pushwoosh: ' + e);
+            return;
+        }
+    }
+
+
+
 	</script>
 <?php } ?>
 
