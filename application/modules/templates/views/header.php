e784557c26dd1dd12c62e82b8584ad8bd4749064
mivida1985 <jortiz@misiva.com.ec>
2017-08-28 10:23:08 -0500

    declaracion espacio

--- application/modules/templates/views/header.php
+++ application/modules/templates/views/header.php
@@ -399,23 +399,24 @@ googletag.defineSlot('/1022247/FE_NEW_RECTANGLE_3', [300, 250], 'div-gpt-ad-1466
     //GS_googleEnableAllServices();
 </script>
 <script type='text/javascript'>
-    //GA_googleAddSlot("ca-pub-2857298972794488", "FE_HP_1");   
+    GA_googleAddSlot("ca-pub-2857298972794488", "FE_HP_1");   
     ////GA_googleAddSlot("ca-pub-2857298972794488", "FE_NEW_HYPERBANNER");
 </script>
 <script type='text/javascript'>
-//GA_googleFetchAds();
+    GA_googleFetchAds();
 </script>
 
 <!-- nuevo formato 20 noticia expandible -->
-<script type='text/javascript' src='http://partner.googleadservices.com/gampad/google_service.js'></script>
 <script type='text/javascript'>
-    GS_googleAddAdSenseService("ca-pub-2857298972794488");
-    GS_googleEnableAllServices();
+    //GS_googleAddAdSenseService("ca-pub-2857298972794488");
+    //GS_googleEnableAllServices();
 </script>
 <script type='text/javascript'>
-   GA_googleAddSlot("ca-pub-2857298972794488", "FE_HP_BRAND");
+   //GA_googleAddSlot("ca-pub-2857298972794488", "FE_HP_BRAND");
 </script>
-<script type='text/javascript'>GA_googleFetchAds();</script>
+<script type='text/javascript'>
+    //GA_googleFetchAds();
+</script>
 
 <!-- Google Tag Manager -->
 <noscript>
