a904e84f06f755b7b392fd62efa57b4fbd056b7c
mivida1985 <jortiz@misiva.com.ec>
2017-08-28 11:39:06 -0500

    cambio js

--- application/modules/templates/views/footerend.php
+++ application/modules/templates/views/footerend.php
@@ -29,11 +29,11 @@
   </style>
 
 <style>
-	.contenedor-expandibl{
-		margin: 25px auto;
-		position: relative;
+	.contenedor-expandible{
 		width: 800px;
-		height: 600px;
+	    height: 600px;
+	    position: relative;
+	    margin: 30px auto;
 	}
 
 	.btn-cerrar-expandible{
@@ -219,7 +219,7 @@
 
 	function unionlayerCoke(){
 		$(".pbl_unionajax-noticia").html("");
-		var contenidoIframe = "<div class='contenedor-expandible'><iframe class='contenedor-video' width='800' height='600' src='http://www.futbolecuador.com/unionlayer/noticia-expandible/coca/expandido/index.html' scrolling='no'></iframe><div class='btn-cerrar-expandible'>Cerrar</div></div>	";
+		var contenidoIframe = "<div class='contenedor-expandible'><iframe class='contenedor-video' width='800' height='600' src='http://www.futbolecuador.com/unionlayer/noticia-expandible/coca/expandido/index.html' scrolling='no' frameborder='0'></iframe><div class='btn-cerrar-expandible'>Cerrar</div></div>	";
 		$(".pbl_unionajax-noticia").html(contenidoIframe);
 		$(".pbl_unionajax-noticia").fadeIn();
 	}
