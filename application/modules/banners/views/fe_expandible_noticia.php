032dd1017b0eeef494adb4bb681c94de6f1dd3c6
mivida1985 <jortiz@misiva.com.ec>
2017-08-28 10:14:38 -0500

    vista

--- application/modules/banners/views/fe_expandible_noticia.php
+++ application/modules/banners/views/fe_expandible_noticia.php
@@ -1,5 +1,5 @@
 <style>
-#expanded-page{
+/*#expanded-page{
 	width: 100%;
     height: 100%;
     background-color: rgba(0,0,0,0.5);
@@ -12,9 +12,15 @@
 .gwd-page-content.expanded.gwd-div-g7oi{
 	margin: 50px auto !important;
 	position: relative !important;
-}
+}*/
 </style>
-
-<div id='FE_HP_BRAND' style='width: 320px; height: auto;margin: 0;padding: 0;line-height: 0;margin-top: -2px;'>
-    <script type='text/javascript'>GA_googleFillSlot("FE_HP_BRAND");</script>
+<!-- /1022247/20-Noticia_Expandible -->
+<div id='div-gpt-ad-1503683104008-0' style='height:340px; width:320px;'>
+    <script>
+        googletag.cmd.push(function() { googletag.display('div-gpt-ad-1503683104008-0'); });
+    </script>
 </div>
+
+<!--<div id='FE_HP_BRAND' style='width: 320px; height: auto;margin: 0;padding: 0;line-height: 0;margin-top: -2px;'>
+    <script type='text/javascript'>GA_googleFillSlot("FE_HP_BRAND");</script>
+</div>-->
