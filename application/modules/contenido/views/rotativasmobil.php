2ce49c9604967c84b34f751acceaa3df57858b3b
mivida1985 <jortiz@misiva.com.ec>
2017-12-11 17:19:33 -0500

    actualizacion en el style para espacio eplayer

--- application/modules/contenido/views/rotativasmobil.php
+++ application/modules/contenido/views/rotativasmobil.php
@@ -1,8 +1,17 @@
 <div class="flexslidermobile preloader " style=" height: 190px; overflow: hidden">
     <ul class="slides ">
         <?php
-        foreach ($rotativasData as &$rotativa) {
-            $linkbody = $rotativa->subtitle;
+         $cont = 1;
+        foreach ($rotativasData as &$rotativa) 
+        {
+        if ($cont == 5){?>
+            <li style="height: 348px;position: relative;" data-thumb="http://www.futbolecuador.com/assets/img/icono-video.jpg">
+                 <script src="//player.performgroup.com/eplayer.js#711e8a5ffd743a6badd37f21a2.1c09jd19fxf3oz6towba3lina" async></script>
+            </li>
+      <?php }
+            else
+            {
+               $linkbody = $rotativa->subtitle;
             if ($linkbody == "")
                 $linkbody = $rotativa->title;
             $link = base_url() . 'site/noticia/' . $this->contenido->_urlFriendly($linkbody) . '/' . $rotativa->id;
@@ -25,7 +34,11 @@
                     </a>
                 </div>
 
-            </li>
+            </li> 
+            }
+
+
+            
         <?php } ?>
     </ul>
 </div>
