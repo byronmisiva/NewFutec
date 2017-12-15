998e510a8b9b7cf01c472737629e98cb7d210b89
mivida1985 <jortiz@misiva.com.ec>
2017-12-15 11:59:35 -0500

    se modifica template para campaña cnt

--- application/modules/contenido/views/marcadorvivo.php
+++ application/modules/contenido/views/marcadorvivo.php
@@ -16,22 +16,22 @@
                                 <img src="http://www.futbolecuador.com/<?php echo $score->hthumb; ?>"
                                      alt="<?php echo $score->hname; ?>">
                             </div>
-                            <div class="col-md-8 col-sm-8 col-xs-8 margen0 h5">
+                            <div class="col-md-8 col-sm-8 col-xs-8 margen0 h5" style="font-family: Arial;">
                                 <?php
                                 if (strlen($score->result) == 0) {
                                     $score->result = " - ";
                                     if ($score->state > 0) $score->result = "0 - 0";
 
                                 };?>
-                                <div class="col-md-5 col-sm-5 col-xs-5 margen0 h5 text-right  ">
+                                <div class="col-md-5 col-sm-5 col-xs-5 margen0 h5 text-right" style="font-family: Arial;">
                                     <?php echo $score->hsname; ?>
                                 </div>
-                                <div class="col-md-2 col-sm-2 col-xs-2 margen0 h5  text-center text-marcador-home">
+                                <div class="col-md-2 col-sm-2 col-xs-2 margen0 h5  text-center text-marcador-home" style="font-family: Arial;">
                                     <?php
                                     //echo str_replace(' ', '', $score->result);
                                     echo   $score->result ; ?>
                                 </div>
-                                <div class="col-md-5 col-sm-5 col-xs-5 margen0 h5 text-left  ">
+                                <div class="col-md-5 col-sm-5 col-xs-5 margen0 h5 text-left" style="font-family: Arial;">
                                     <?php echo $score->asname; ?>
                                 </div>
                             </div>
@@ -48,15 +48,16 @@
                             <p><?php echo $active; ?> </p>
                         </div>-->
                         <!--<div class="col-md-5 col-sm-5 col-xs-5 h6  text-blue1">-->
-                        <div class="col-md-12 col-sm-12 col-xs-12 h6  text-blue1 text-center">
-                            <p> <?php setlocale(LC_ALL, "es_ES");
+                        <div class="col-md-12 col-sm-12 col-xs-12 h6  text-blue1 text-center" style="color:#ffffff;">
+                            <p style="color:#ffffff;"> <?php setlocale(LC_ALL, "es_ES");
                                 echo ucwords (utf8_encode(strftime("%B %d %Hh%M", strtotime($score->date_match))));?>
                             </p>
-                            <p><?php echo $active; ?>
-                                <?php
-                                $states = array(0 => 'No Iniciado', 1 => 'Primer Tiempo', 2 => 'Fin del Primer Tiempo', 3 => 'Segundo Tiempo', 4 => 'Fin del Segundo Tiempo',
+                            <!--<p><?php echo $active; ?>
+                            <?php
+                            $states = array(0 => 'No Iniciado', 1 => 'Primer Tiempo', 2 => 'Fin del Primer Tiempo', 3 => 'Segundo Tiempo', 4 => 'Fin del Segundo Tiempo',
                                     5 => 'Primer Extra', 6 => 'Segundo Extra', 7 => 'Penales', 8 => 'Final del Partido');
-                                echo $states[$score->state];?></p>
+                                echo $states[$score->state];
+                                    ?></p>-->
                         </div>
 
                         <div class="col-md-12 col-md-offset-0 col-sm-12 col-xs-12 col-sm-offset-1 h6 text-blue1 margen0">
