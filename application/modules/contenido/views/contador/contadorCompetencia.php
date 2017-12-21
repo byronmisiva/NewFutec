c8bcf8aa7cd9a8029d510a6cd7418556d185fd98
mivida1985 <jortiz@misiva.com.ec>
2017-12-21 14:05:05 -0500

    contador

---
+++ application/modules/contenido/views/contador/contadorCompetencia.php
@@ -0,0 +1,15 @@
+<?php 
+    $valor = rand(0, 1);
+    if ($valor == 0)
+    {?>
+        <div class="col-md-12 col-xs-12 separador10 margen0r">
+            <iframe src="https://appss.misiva.com.ec/marcador/" scrolling="no" frameborder="0" style="height: 570px;width: 100%;float: left;"></iframe>
+        </div>
+  <?php  
+    }
+    else
+    { ?>
+        <div class="col-md-12 col-xs-12 separador10 margen0r">
+            <iframe src="https://appss.misiva.com.ec/marcador/lduvstuni/" scrolling="no" frameborder="0" style="height: 570px;width: 100%;float: left;"></iframe>
+        </div>
+<?php } ?>
