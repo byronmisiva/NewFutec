19d05b4266d16dd8905d9e21254e8f65a108c397
mivida1985 <jortiz@misiva.com.ec>
2017-12-21 14:00:30 -0500

    creacion iframe eplayer para movil

---
+++ application/modules/contenido/views/eplayer.php
@@ -0,0 +1,6 @@
+ <div class="row">
+ 	<div class="col-sx-12">
+ 		<script src="//player.performgroup.com/eplayer.js#e962aa27b4c760ae4b3feaff6a.1c09jd19fxf3oz6towba3lina" async></script>	
+ 	</div>
+ </div>
+ 
