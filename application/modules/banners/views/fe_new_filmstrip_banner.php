3803e9a08367d6c397fa3a91fe7da178f9ecdaf3
mivida1985 <jortiz@misiva.com.ec>
2017-12-26 16:43:56 -0500

    actualizacion de espacio

--- application/modules/banners/views/fe_new_filmstrip_banner.php
+++ application/modules/banners/views/fe_new_filmstrip_banner.php
@@ -1,8 +1,3 @@
-<?php
-	$this->load->module('banners');
-    echo $this->banners->eplayer();
-?>
-
 <!-- /1022247/FE_NEW_FILMSTRIP_BANNER -->
 <div id='div-gpt-ad-1439997438966-0' style="height:auto; width:300px;">
     <script type='text/javascript'>
